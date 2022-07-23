9d7297fa20bb784989c235afea1b29d081d4fce2
karam Choi <86296569+chlrkfka@users.noreply.github.com>
2022-07-23 14:05:46 +0900

    [Silver V] Title: 단어 정렬, Time: 28 ms, Memory: 2528 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.cc
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.cc
@@ -8,54 +8,13 @@ typedef struct Word {
 	int len;
 }Word;
 
-//int partition(Word* wlist, int p, int r)
-//{
-//	int i, j, k = 0;
-//	Word temp;
-//
-//	j = p;
-//	i = p - 1;
-//
-//	for (; j < r; j++) {
-//		if (wlist[j].len < wlist[r].len) {
-//			i++;
-//			temp = wlist[i];
-//			wlist[i] = wlist[j];
-//			wlist[j] = temp;
-//		}
-//		else if (wlist[j].len == wlist[r].len) {
-//			if(strcmp(wlist[j].w, wlist[r].w)==-1){
-//				i++;
-//				temp = wlist[i];
-//				wlist[i] = wlist[j];
-//				wlist[j] = temp;
-//			}
-//		}		
-//	}
-//	i++;
-//	temp = wlist[i];
-//	wlist[i] = wlist[r];
-//	wlist[r] = temp;
-//
-//	return i;
-//}
-//void quickSort(Word* wlist, int p, int r)
-//{
-//	int q;
-//
-//	if (p < r) {
-//		q = partition(wlist, p, r);
-//		quickSort(wlist, p, q - 1);
-//		quickSort(wlist, q + 1, r);
-//	}
-//}
 int compare(const void* a, const void* b) {
-	Word s1 = *(Word*)a, s2 = *(Word*)b;
-	if (s1.len < s2.len)
+	Word w1 = *(Word*)a, w2 = *(Word*)b;
+	if (w1.len < w2.len)
 		return -1;
-	else if (s1.len > s2.len)
+	else if (w1.len > w2.len)
 		return 1;
-	return strcmp(s1.w, s2.w);
+	return strcmp(w1.w, w2.w);
 }
 
 
@@ -71,8 +30,8 @@ int main()
 		scanf("%s", wlist[i].w);
 		wlist[i].len = strlen(wlist[i].w);
 	}
+	
 	qsort(wlist, n, sizeof(Word), compare);
-	//quickSort(wlist, 0, n);
 
 	printf("%s\n", (wlist[0].w));
 
