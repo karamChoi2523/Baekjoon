7ae2f16e18c355def9fefa5b4a4ced6d7ff4d138
karam Choi <86296569+karamChoi2523@users.noreply.github.com>
2022-10-10 01:14:17 +0900

    [Silver II] Title: 로또, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.cc
+++ 백준/Silver/6603. 로또/로또.cc
@@ -2,14 +2,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void pick(int items[], int n, int picked[], int m, int toPick)
+//조합
+void pick(int* item, int n, int* picked, int m, int toPick)
 {
-	int i, lastIndex, smallest, picked_index;
+	int i, lastIndex, smallest;
 
 	if (toPick == 0) {
-		for (i = 0; i < m; i++) {
-			printf("%d ", items[picked[i]]);
-		}
+		for (i = 0; i < m; i++)
+			printf("%d ", item[picked[i]]);
 		printf("\n");
 		return;
 	}
@@ -22,10 +22,10 @@ void pick(int items[], int n, int picked[], int m, int toPick)
 
 	for (i = smallest; i < n; i++) {
 		picked[lastIndex + 1] = i;
-		pick(items, n, picked, m, toPick - 1);
+		pick(item, n, picked, m, toPick - 1);
 	}
-
 }
+
 int main()
 {
 	int k;
@@ -45,17 +45,18 @@ int main()
 	while (len > 0) {
 		k = arr[i];
 		picked = (int*)malloc(sizeof(int) * k);
+
 		for (j = 1; j <= k; j++) {
-			s[j-1] = arr[i+j];
+			s[j - 1] = arr[i + j];
 		}
 		if (i != 0)
 			printf("\n");
 		pick(s, k, picked, 6, 6);
-		i += (k+1);
+		i += (k + 1);
 		len -= (k + 1);
 
 		free(picked);
 	}
-	
+
 	return 0;
 }
