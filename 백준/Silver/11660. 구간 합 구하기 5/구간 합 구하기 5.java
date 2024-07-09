56d887a2cdc042dbc852db05fa6151c33bdc145c
karam Choi <86296569+karamChoi2523@users.noreply.github.com>
2024-07-09 23:26:04 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 984 ms, Memory: 128356 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -5,6 +5,7 @@ import java.util.StringTokenizer;
 
 public class Main {
 	static StringBuilder sb = new StringBuilder();
+	static int[][] board;
 	static int[][] d;
 	
 	public static void main(String[] args) throws IOException {
@@ -14,37 +15,32 @@ public class Main {
 		int n = Integer.valueOf(st.nextToken());
 		int m = Integer.valueOf(st.nextToken());
 		
+		board = new int[n+1][n+1];
 		d = new int[n+1][n+1];
 		
 		for(int i=1;i<n+1;i++) {
 			st = new StringTokenizer(br.readLine());
 			
 			for(int j=1;j<n+1;j++)
-				d[i][j] = d[i][j-1] + Integer.parseInt(st.nextToken());
+				board[i][j] = Integer.parseInt(st.nextToken());
 		}
+		
+		for(int i=1;i<n+1;i++)
+			for(int j=1;j<n+1;j++)
+				d[i][j] = board[i][j]+d[i-1][j]+d[i][j-1]-d[i-1][j-1];
 		
 		for(int i=0;i<m;i++) {
 			st = new StringTokenizer(br.readLine());
 			//x1, y1, x2, y2
-			int x1 = Integer.valueOf(st.nextToken());
-			int y1 = Integer.valueOf(st.nextToken());
-			int x2 = Integer.valueOf(st.nextToken());
-			int y2 = Integer.valueOf(st.nextToken());
+			int sx = Integer.valueOf(st.nextToken());
+			int sy = Integer.valueOf(st.nextToken());
+			int ex = Integer.valueOf(st.nextToken());
+			int ey = Integer.valueOf(st.nextToken());
 			
-			dp(x1, y1, x2, y2);
+			int answer = d[ex][ey] - d[ex][sy-1] -d[sx-1][ey] + d[sx-1][sy-1];
+			sb.append(answer+"\n");
 		}
 		
 		System.out.println(sb.toString());	
 	}
-
-	private static void dp(int sx, int sy, int ex, int ey) {
-		int answer = 0;
-		
-		for (int i=sx; i<ex+1; i++) {
-            answer += (d[i][ey] - d[i][sy-1]);
-        }
-		
-		sb.append(answer+"\n");
-	}
-
 }
