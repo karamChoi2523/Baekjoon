89f19c9836df4a0f15344af2aad5b16981017153
karam Choi <86296569+karamChoi2523@users.noreply.github.com>
2024-07-24 23:49:25 +0900

    [Silver I] Title: 신입 사원, Time: 1644 ms, Memory: 300876 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.java
+++ 백준/Silver/1946. 신입 사원/신입 사원.java
@@ -6,17 +6,17 @@ import java.util.StringTokenizer;
 public class Main {
 	
 	static class Person implements Comparable<Person> {
-		int score1;
-		int score2;
+		int rank1;	//서류 순위
+		int rank2;	//면접 순위
 		
-		public Person(int s1, int s2) {
-			score1 = s1;
-			score2 = s2;
+		public Person(int rank1, int rank2) {
+			this.rank1 = rank1;
+			this.rank2 = rank2;
 		}
 
 		@Override
 		public int compareTo(Person o) {
-			return this.score1 - o.score1;
+			return this.rank1 - o.rank1;
 		}
 	}
 
@@ -44,14 +44,15 @@ public class Main {
 			Collections.sort(list);
 			
 			int cnt=1;
-			int min = list.get(0).score2;
+			int pre = list.get(0).rank2;	//서류 점수 가장 높은 사람의 면접 순위
 			
 			for(int i=1;i<n;i++) {
 				Person curr = list.get(i);
 				
-				if(curr.score2 < min) {
+				//이전 합격자보다 면접 시험 못 본 사람 탈락
+				if(curr.rank2 < pre) {	//이전 사람보다 면접 순위가 높으면
 					cnt++;
-					min = curr.score2;
+					pre = curr.rank2;
 				}
 			}
 			
