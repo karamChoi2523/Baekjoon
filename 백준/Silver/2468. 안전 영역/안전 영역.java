777149d97cf4dff541582309011f2f1b1d2b6a8e
karam Choi <86296569+karamChoi2523@users.noreply.github.com>
2024-08-01 22:49:23 +0900

    [Silver I] Title: 안전 영역, Time: 260 ms, Memory: 19032 KB -BaekjoonHub

---
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int n;
+	static int[][] board;
+	static boolean[][] visited;
+	
+	static int[] dx = new int[] {-1,1,0,0};
+	static int[] dy = new int[] {0,0,-1,1};
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		n = Integer.valueOf(st.nextToken());
+		
+		board = new int[n][n];
+	
+		int maxH = Integer.MIN_VALUE;	//가장 높은 곳도 잠기면 0개이다
+		for(int i=0;i<n;i++) {
+			st = new StringTokenizer(br.readLine());
+			
+			for(int j=0;j<n;j++) {
+				board[i][j] = Integer.valueOf(st.nextToken());
+				maxH = Math.max(maxH, board[i][j]);
+			}
+		}
+		
+		//잠긴 곳이 없으면 안전 영역 개수는 1개
+		//모두 잠기면 0개
+		int max = 0;
+		for(int h=0;h<maxH+1;h++) {
+			visited = new boolean[n][n];
+			
+			int cnt = 0;
+			for(int i=0;i<n;i++)
+				for(int j=0;j<n;j++)
+					if(!visited[i][j] && board[i][j] >h)
+						cnt += dfs(i,j,h);
+			max = Math.max(max, cnt);
+		}
+		
+		System.out.println(max);
+	}
+
+	private static int dfs(int x, int y, int height) {
+		visited[x][y] = true;
+		
+		for(int i=0;i<4;i++) {
+			int nx = x+dx[i];
+			int ny = y+dy[i];
+			
+			if(nx>=0 && nx<n && ny>=0 && ny<n) {
+				if(!visited[nx][ny] && board[nx][ny] > height) {
+					dfs(nx, ny, height);
+				}
+			}
+		}
+		
+		return 1;
+	}
+
+
+}
