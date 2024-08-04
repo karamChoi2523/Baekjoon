fcc41402328054e99dea3662e2a331b012dae6ea
karam Choi <86296569+karamChoi2523@users.noreply.github.com>
2024-08-04 13:29:31 +0900

    Update 숨바꼭질.java

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,53 +1,57 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 	static int n, k;
-	static boolean[] visited = new boolean[100001];
-	static int countArr[] = new int[100001];
-    
+	static boolean[] visited;
+	static int[] arr;
+
 	public static void main(String[] args) throws IOException {
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(bf.readLine());
-		n = Integer.parseInt(st.nextToken());
-		k = Integer.parseInt(st.nextToken());
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		bfs(n);
-
-		System.out.println(countArr[k]-1);
+		n = Integer.valueOf(st.nextToken());
+		k = Integer.valueOf(st.nextToken());
+	
+		arr = new int[100001];
+		visited = new boolean[100001];
+		
+		Arrays.fill(arr, 100001);
+		
+		bfs(n);	//count를 세야 하므로 bfs
+		
+		System.out.println(arr[k]);
 	}
-	public static void bfs(int n) {
+
+	private static void bfs(int start) {
 		Queue<Integer> q = new LinkedList<>();
-		q.add(n);
-		visited[n] = true;
-		countArr[n]=1;
-
+		visited[start] = true;
+		q.offer(start);
+		arr[start] = 0;
+				
 		while(!q.isEmpty()) {
-			int x = q.poll();	//현 위치
-
-			if(x==k) {
-				return;
-			}
+			int curr = q.poll();
 			
-			if(x-1>=0 && !visited[x-1]) {
-				visited[x-1]=true;
-				countArr[x-1] = countArr[x]+1;
-				q.add(x-1);
+			if(curr-1 >=0 && !visited[curr-1]) {
+				q.offer(curr-1);
+				visited[curr-1] = true;
+				arr[curr-1] = Math.min(arr[curr-1], arr[curr]+1);
 			}
-			if(x+1<=100000 && !visited[x+1]) {
-				visited[x+1]=true;
-				countArr[x+1] = countArr[x]+1;
-				q.add(x+1);
+			if(curr+1 <= 100000 && !visited[curr+1]) {
+				q.offer(curr+1);
+				visited[curr+1] = true;
+				arr[curr+1] = Math.min(arr[curr+1], arr[curr]+1);
 			}
-			if(x*2<=100000 && !visited[x*2]) {
-				visited[x*2]=true;
-				countArr[x*2] = countArr[x]+1;
-				q.add(x*2);
+			if(curr*2 <= 100000 && !visited[curr*2]) {
+				q.offer(curr*2);
+				visited[curr*2] = true;
+				arr[curr*2] = Math.min(arr[curr*2], arr[curr]+1);
 			}
 		}
 	}
+
 }
