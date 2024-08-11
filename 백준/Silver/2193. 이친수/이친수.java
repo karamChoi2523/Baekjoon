9818b76b35d65674085b5b3e4e6ccbc7678487c1
karam Choi <86296569+karamChoi2523@users.noreply.github.com>
2024-08-11 15:27:32 +0900

    Update 이친수.java

--- 백준/Silver/2193. 이친수/이친수.java
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -2,15 +2,20 @@ import java.io.*;
 
 public class Main {
     static long[][] dp;
+    static long[] d;
     
 	public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.valueOf(br.readLine());
         dp = new long[91][2];
+        d = new long[91];
         
-        solution();
-        System.out.println(dp[n][0]+dp[n][1]);
+        //solution();
+        //System.out.println(dp[n][0]+dp[n][1]);
+        
+        solution2();
+        System.out.println(d[n]);
 	}
 
 	static void solution() {
@@ -22,5 +27,11 @@ public class Main {
 			dp[i][0] = dp[i-1][0]+dp[i-1][1];
 			dp[i][1] = dp[i-1][0];
 		}
+	}
+	
+	static void solution2() {
+		d[1] = 1;
+		for(int i=2;i<91;i++)
+			d[i] = d[i-1]+d[i-2];
 	}
 }
