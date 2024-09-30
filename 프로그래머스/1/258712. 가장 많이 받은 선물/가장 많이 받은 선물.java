2e812bc81ded036a87df90ac8d91c1324529c998
karam Choi <86296569+karamChoi2523@users.noreply.github.com>
2024-09-30 11:29:51 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 24.88 ms, Memory: 99 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
@@ -1,48 +1,26 @@
 import java.util.*;
 
 class Solution {
-    static class Friend{
-		int a;
-		int b;
-		int c;
-		
-		public Friend(int a,int b,int c) {
-			this.a = a;
-			this.b = b;
-			this.c = c;
-		}
-	}
     public int solution(String[] friends, String[] gifts) {
         int answer = 0;
+        
+        Map<String,Integer> map = new HashMap<>();
+        for(int i=0;i<friends.length;i++)
+        	map.put(friends[i], i);
      
         int[][] board = new int[friends.length][friends.length];
-        int[] ans = new int[friends.length];
+        int[] index = new int[friends.length];	//선물지수
         
         for(String record : gifts) {
         	String[] rr = record.split(" ");
         	
-        	int a=-1, b=-1;
-        	
-        	for(int i=0;i<friends.length;i++) {
-        		if(friends[i].equals(rr[0]))
-        			a = i;
-        		else if(friends[i].equals(rr[1]))
-        			b = i;
-        		
-        		if(a!=-1 && b!=-1)
-        			break;
-        	}
-        	
-        	board[a][b]++;
-        }
-        
-        ArrayList<Friend> list = new ArrayList<>();
-        for(int i=0;i<friends.length;i++) {
-        	int[] arr = findRecord(board, i);
-        	list.add(new Friend(arr[0],arr[1],arr[2]));
+        	index[map.get(rr[0])]++;
+        	index[map.get(rr[1])]--;
+        	board[map.get(rr[0])][map.get(rr[1])]++;
         }
         
         for(int i=0;i<friends.length;i++) {
+        	int cnt = 0;
         	for(int j=0;j<friends.length;j++) {
         		if(i!=j) {
         			int a = board[i][j];
@@ -50,39 +28,15 @@ class Solution {
         			
         			
         			if(a>b)
-        				ans[i]++;
-        			else if(a<b)
-        				ans[j]++;
-        			else {
-        				int ta = list.get(i).c;
-        				int tb = list.get(j).c;
-        				
-        				if(ta>tb)
-        					ans[i]++;
-        				else if(ta<tb)
-        					ans[j]++;
-        			}
+        				cnt++;
+        			else if(board[i][j]==board[j][i] && index[i]>index[j])
+        				cnt++;
         		}
+        		answer = Math.max(answer, cnt);
         	}
         }
         
-        for(int i=0;i<ans.length;i++)
-        	answer = Math.max(answer, ans[i]);
         
-        
-        return answer/2;
+        return answer;
     }
-    private static int[] findRecord(int[][] board, int f) {	//선물 지수 반환
-		int a = 0;	//받은
-		int b = 0;	//준
-		for(int i=0;i<board[f].length;i++) {
-			b+=board[f][i];
-		}
-		
-		for(int i=0;i<board.length;i++)
-			a+=board[i][f];
-
-		return new int[] {b, a, b-a};
-	}
-	
 }
