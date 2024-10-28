632f914ff93253b270691cc662de074236592816
karam Choi <86296569+karamChoi2523@users.noreply.github.com>
2024-10-28 23:24:40 +0900

    [Gold V] Title: 최소비용 구하기, Time: 404 ms, Memory: 51564 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -1,91 +1,80 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
+
 
 public class Main {
-	static class Node implements Comparable<Node> {
+	static class Edge{
+		int to;
 		int cost;
-		int node;
 		
-		public Node(int c, int n) {
+		public Edge(int t, int c) {
+			to = t;
 			cost = c;
-			node = n;
-		}
-
-		@Override
-		public int compareTo(Node o) {
-			return this.cost - o.cost;
 		}
 	}
 	
-	static ArrayList<Node>[] graph;
-	//static boolean[] visited;
-	static int[] dist;
-	static int start, end;
+	static int INF = (int)1e9;
 	
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		int n = Integer.valueOf(br.readLine());
 		int m = Integer.valueOf(br.readLine());
 		
-		graph = new ArrayList[n+1];
-		//visited = new boolean[n+1];
-		dist = new int[n+1];
+		solutionA(br, n, m);
+		solutionB(br, n, m);
+	}
+	private static void solutionA(BufferedReader br, int n, int m) throws IOException {
+		// 인접리스트
+		ArrayList<ArrayList<Edge>> a = new ArrayList<>(); 	//인접리스트
+		int[] dist = new int[n+1];
+		boolean[] visited = new boolean[n+1];
 		
+		Arrays.fill(dist, INF);
 		for(int i=0;i<n+1;i++)
-			graph[i] = new ArrayList<>();
+			a.add(new ArrayList<>());
 		
 		for(int i=0;i<m;i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			
-			int s = Integer.valueOf(st.nextToken());
-			int e = Integer.valueOf(st.nextToken());
+
+			int from = Integer.valueOf(st.nextToken());
+			int to = Integer.valueOf(st.nextToken());
 			int cost = Integer.valueOf(st.nextToken());
 			
-			graph[s].add(new Node(cost, e));
+			a.get(from).add(new Edge(to, cost));
 		}
 		
 		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int start = Integer.valueOf(st.nextToken());
+		int end = Integer.valueOf(st.nextToken());
 		
-		start = Integer.valueOf(st.nextToken());
-		end = Integer.valueOf(st.nextToken());
+		dist[start] = 0;
 		
-		Arrays.fill(dist, 100000001);
-		
-		dijkstra(start);
+		for(int k=0;k<n-1;k++) {
+			int temp = INF+1;
+			int x = -1;
+			
+			for(int i=1;i<n+1;i++) {
+				if(!visited[i] && dist[i] < temp) {
+					temp = dist[i];
+					x = i;
+				}
+			}
+			
+			visited[x] = true;	//선택한 정점
+			for(int i=0;i<a.get(x).size();i++) {
+				int to = a.get(x).get(i).to;
+				int cost = a.get(x).get(i).cost;
+				
+				if(dist[to] > dist[x]+cost)
+					dist[to] = dist[x]+cost;
+			}
+		}
 		
 		System.out.println(dist[end]);
 	}
-	private static void dijkstra(int start) {
-		PriorityQueue<Node> q = new PriorityQueue<>();
-		q.add(new Node(0, start));
-		dist[start]=0;
-		
-		while(!q.isEmpty()) {
-			Node curr = q.poll();
-			/*
-			if(visited[curr.node])
-				continue;
-			
-			visited[curr.node] = true;
-			*/
-			if(dist[curr.node] < curr.cost)
-				continue;
-			
-			for(Node next : graph[curr.node]) {
-				int cost = next.cost+curr.cost;
-				
-				if(cost < dist[next.node]) {
-					dist[next.node] = cost;
-					q.add(new Node(cost, next.node));
-				}				
-			}
-		}
+	private static void solutionB(BufferedReader br, int n, int m) throws IOException {
+		// 인접행렬
 	}
-
 }
