a8638ceabf6ee9c5c23630ca6c08978764eff356
karam Choi <86296569+karamChoi2523@users.noreply.github.com>
2025-02-05 23:33:14 +0900

    [Silver I] Title: RGB거리, Time: 120 ms, Memory: 14568 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,40 +1,37 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static int[][] cost;
-	static int[][] dp;
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int n = sc.nextInt();
+		int n = Integer.valueOf(br.readLine());
+
+		int[][] dp = new int[n+1][3];
+		int[][] cost = new int[n+1][3];
 		
-		cost = new int[n][3];	//rgb
-		dp = new int[n][3];
-		
-		for(int i=0;i<n;i++) {
-			for(int j=0;j<3;j++)
-				cost[i][j] = sc.nextInt();
+		for(int i=1;i<n+1;i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			int j=0;
+			while(st.hasMoreTokens()) {
+				cost[i][j++] = Integer.valueOf(st.nextToken());
+			}
 		}
 		
-		dp[0][0] = cost[0][0];
-		dp[0][1] = cost[0][1];
-		dp[0][2] = cost[0][2];
+		dp[1][0] = cost[1][0];
+		dp[1][1] = cost[1][1];
+		dp[1][2] = cost[1][2];
 		
-		System.out.println(Math.min(paint(n-1, 0), Math.min(paint(n-1, 1), paint(n-1, 2))));
-	}
-
-	private static int paint(int n, int color) {
-		
-		if(dp[n][color]==0) {
-			if(color==0)
-				dp[n][0] = Math.min(paint(n-1, 1), paint(n-1, 2))+cost[n][0];
-			if(color==1)
-				dp[n][1] = Math.min(paint(n-1, 0), paint(n-1, 2))+cost[n][1];
-			if(color==2)
-				dp[n][2] = Math.min(paint(n-1, 0), paint(n-1, 1))+cost[n][2];
+		for(int i=2;i<n+1;i++) {
+			dp[i][0] = Math.min(dp[i-1][1], dp[i-1][2])+cost[i][0];
+			dp[i][1] = Math.min(dp[i-1][0], dp[i-1][2])+cost[i][1];
+			dp[i][2] = Math.min(dp[i-1][0], dp[i-1][1])+cost[i][2];
 		}
 		
-		return dp[n][color];
+		System.out.println(Math.min(dp[n][0], Math.min(dp[n][1], dp[n][2])));
+		
 	}
-
+	
 }
