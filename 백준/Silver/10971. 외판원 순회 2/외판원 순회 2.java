7cb2f26e6e418a27f91ce3a2f20e4cf6e20e44bf
karam Choi <86296569+karamChoi2523@users.noreply.github.com>
2025-02-26 23:25:45 +0900

    [Silver II] Title: 외판원 순회 2, Time: 460 ms, Memory: 14672 KB -BaekjoonHub

---
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static long ans = Long.MAX_VALUE;
+	static int N;
+	static int[][] W;
+	static boolean[] visited;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		N = Integer.valueOf(br.readLine());
+		visited = new boolean[N];
+		W = new int[N][N];
+		
+		
+		for(int i=0;i<N;i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for(int j=0;j<N;j++)
+				W[i][j] = Integer.valueOf(st.nextToken());
+		}
+		
+		for(int i=0;i<N;i++) {
+			visited[i] = true;
+			solution(1, i, i, 0);
+			visited[i] = false;
+		}
+		
+		System.out.println(ans);
+	}
+	private static void solution(int depth, int start, int preNode, long cost) {
+		if(depth==N) {
+			if(W[preNode][start]==0) return;
+			ans = Math.min(cost+W[preNode][start], ans);
+		}
+		
+		for(int i=0;i<N;i++) {
+			if(i!=start && W[preNode][i]!=0 && !visited[i]) {
+				visited[i] = true;
+				solution(depth+1, start, i, cost+W[preNode][i]);
+				visited[i] = false;
+			}
+		}
+	}
+}
