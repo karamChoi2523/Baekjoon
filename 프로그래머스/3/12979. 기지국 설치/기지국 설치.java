6108d0ad76151c64c9d1c53007bf3277c927c3c0
karam Choi <86296569+karamChoi2523@users.noreply.github.com>
2025-04-01 18:13:31 +0900

    [level 3] Title: 기지국 설치, Time: 1.04 ms, Memory: 55.3 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
@@ -1,9 +1,10 @@
 class Solution {
-    static private int bsearch(int begin, int end, int w){
-        int res = (end-begin+1)/(w*2+1);
-        if((end-begin+1)%(w*2+1)>0) res++;
+    static private int sol(int begin, int end, int w){
+        int ans = (end-begin+1)/(w*2+1);
+        if((end-begin+1)%(w*2+1) > 0)
+            ans++;
         
-        return res;
+        return ans;
     }
     public int solution(int n, int[] stations, int w) {
         int answer = 0;
@@ -12,11 +13,12 @@ class Solution {
         
         for(int i=0;i<stations.length;i++){
             if(begin < stations[i]-w)
-                answer += bsearch(begin, stations[i]-w-1, w);
+                answer+=sol(begin, stations[i]-w-1, w);
             begin = stations[i]+w+1;
         }
+        
         if(stations[stations.length-1]+w<n)
-            answer += bsearch(stations[stations.length-1]+w+1, n, w);
+            answer+=sol(stations[stations.length-1]+w+1, n, w);
 
         return answer;
     }
