427fa369938e6116abc80549b440bc25f995929a
karam Choi <86296569+karamChoi2523@users.noreply.github.com>
2025-04-01 16:37:35 +0900

    [level 3] Title: 네트워크, Time: 0.29 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,23 +1,25 @@
-import java.util.*;
-
 class Solution {
+    static boolean[] visited;
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        boolean[] visited = new boolean[n];
         
-        for(int i=0;i<n;i++)
+        visited = new boolean[n];
+        
+        for(int i=0;i<n;i++){
             if(!visited[i]){
-                dfs(n, visited, computers, i);
+                dfs(i, n, computers);
                 answer++;
             }
+        }
+        
         return answer;
     }
-    
-    static void dfs(int n, boolean[] visited, int[][] computers, int index){
-        visited[index] = true;
-        
-        for(int i=0;i<n;i++)
-            if(computers[index][i]==1 && !visited[i])
-                dfs(n, visited, computers, i);
+    static void dfs(int index, int n, int[][] computers){        
+        for(int i=0;i<n;i++){
+            if(computers[index][i]==1 && !visited[i]){
+                visited[i] = true;
+                dfs(i, n, computers);
+            }
+        }
     }
 }
