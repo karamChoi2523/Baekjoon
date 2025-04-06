a193bf85caf2ffcccaf558ec99258670d5cb5b4d
karam Choi <86296569+karamChoi2523@users.noreply.github.com>
2025-04-06 16:28:49 +0900

    [level 2] Title: 숫자의 표현, Time: 0.19 ms, Memory: 53.7 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -2,20 +2,10 @@ class Solution {
     public int solution(int n) {
         int answer = 0;
         
-        answer++;
-        for(int i=1;i<=n/2+1;i++){
-            int sum = i;
-            for(int j=i+1;j<n;j++){
-                sum+=j;
-                if(sum > n) break;
-                
-                if(sum==n){
-                    answer++;
-                    break;
-                }
-            }
-        }       
-        
+        //주어진 수의 홀수 약수의 개수와 같음
+        for(int i=1;i<=n;i+=2){
+            if(n%i==0) answer++;
+        }
         
         return answer;
     }
