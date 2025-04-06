b98b10f248bb92653dccd63a55182a61527192e4
karam Choi <86296569+karamChoi2523@users.noreply.github.com>
2025-04-06 12:39:26 +0900

    [level 2] Title: 올바른 괄호, Time: 21.36 ms, Memory: 56.4 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -3,18 +3,31 @@ import java.util.*;
 class Solution {
     boolean solution(String s) {
         boolean answer = true;
-        Stack<Integer> stack = new Stack<>();
         
+        Stack<String> stack = new Stack<>();
+
         for(int i=0;i<s.length();i++){
             char c = s.charAt(i);
             
-            if(c=='(') stack.push(1);
-            else{
-                if(stack.size()==0) return false;
-                stack.pop();
+            if(c=='('){
+                stack.push(String.valueOf(c));
+            }else{
+                boolean check = false;
+                while(true){
+                    if(stack.isEmpty()) break;
+                    String pre = stack.pop();
+                    
+                    if(pre.equals("(")){
+                        check = true;
+                        break;
+                    }
+                }
+                if(!check) return false;
             }
         }
-        if(stack.size()!=0) return false;
+        
+        if(!stack.isEmpty()) return false;
+
         return answer;
     }
 }
