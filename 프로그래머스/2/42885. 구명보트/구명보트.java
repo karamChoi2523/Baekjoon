c27429041fa95b71e5789f57b053bbf9b4a403f5
karam Choi <86296569+karamChoi2523@users.noreply.github.com>
2025-04-08 21:52:51 +0900

    [level 2] Title: 구명보트, Time: 22.13 ms, Memory: 57.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        
+        Arrays.sort(people);
+        int left = 0;
+        
+        for(int i=people.length-1;i>=left;i--){
+            if(people[i]+people[left]<=limit){
+                answer++;
+                left++;
+            }else
+                answer++;
+        }
+        
+        return answer;
+    }
+}
