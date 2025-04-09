162650cf3402210e95c619faf28fb6578e39319a
karam Choi <86296569+karamChoi2523@users.noreply.github.com>
2025-04-09 17:22:52 +0900

    [level 2] Title: 구명보트, Time: 13.66 ms, Memory: 58.7 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/README.md
+++ 프로그래머스/2/42885. 구명보트/README.md
@@ -1,10 +1,10 @@
 # [level 2] 구명보트 - 42885 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885) 
 
 ### 성능 요약
 
-메모리: 58.6 MB, 시간: 12.44 ms
+메모리: 58.7 MB, 시간: 13.66 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 08일 21:55:52
+2025년 04월 09일 17:22:50
 
 ### 문제 설명
 
--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -5,22 +5,18 @@ class Solution {
         int answer = 0;
         
         Arrays.sort(people);
-        int left = 0;
-        
-        for(int i=people.length-1;i>=left;i--){
-            if(people[i]+people[left]<=limit){
-                answer++;
-                left++;
-            }else
-                answer++;
-        }
         
         int i=0,j=people.length-1;
-        for(;i<j;--j){
-            if(people[i]+people[j]<=limit)
+        int cnt=0;
+        
+        for(;i<j;j--){
+            if(people[i]+people[j]<=limit){
+                cnt++;
                 i++;
+            }
         }
         
-        return answer = people.length-i;
+        answer = people.length;
+        return answer-cnt;
     }
 }
