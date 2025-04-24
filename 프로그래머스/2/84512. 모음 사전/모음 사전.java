866dc427c7cc34db752819f4b13eda5dc82706bf
karam Choi <86296569+karamChoi2523@users.noreply.github.com>
2025-04-24 22:22:34 +0900

    [level 2] Title: 모음 사전, Time: 9.50 ms, Memory: 82.6 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,28 +1,22 @@
 import java.util.*;
 
 class Solution {
-    static String[] arr = new String[]{"A","E","I","O","U"};
+    static String[] arr = {"A", "E", "I", "O", "U"};
     static ArrayList<String> total = new ArrayList<>();
     public int solution(String word) {
         int answer = 0;
         
-        for(int i=1;i<=5;i++){
-            dfs(i, 0, "");
-        }
-        
-        Collections.sort(total);
-        
-        return answer = total.indexOf(word)+1;
+        dfs("", 0);
+        return answer = total.indexOf(word);
     }
     
-    static private void dfs(int target, int step, String str){
-        if(target==step){
-            total.add(str);
-            return;
-        }
+    static private void dfs(String curr, int step){
+        if(step>5) return;
+        
+        total.add(curr);
         
         for(int i=0;i<arr.length;i++){
-            dfs(target, step+1, str+arr[i]);
+            dfs(curr+arr[i], step+1);
         }
     }
 }
