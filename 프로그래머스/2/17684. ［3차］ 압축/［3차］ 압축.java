4d7d467ec05e84d0bffc9f5294317d5a54dee5cf
karam Choi <86296569+karamChoi2523@users.noreply.github.com>
2025-07-09 22:26:30 +0900

    [level 2] Title: [3차] 압축, Time: 122.38 ms, Memory: 125 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
@@ -1,44 +1,38 @@
 import java.util.*;
 
 class Solution {
-	static ArrayList<Integer> ans;
-	static int index;
     public int[] solution(String msg) {
         int[] answer = {};
+        ArrayList<Integer> ans = new ArrayList<>();
+        HashMap<String, Integer> hMap = new HashMap<>();
         
-		HashMap<String, Integer> dict = new HashMap<>();
-		
-		int idx = 1;
-		for(char c='A';c<='Z';c++)
-			dict.put(String.valueOf(c), idx++);
-		
-		ans = new ArrayList<>();
-		for(int i=0;i<msg.length();i++) {
-			String key = makeStr(i, msg, dict);
-			if(index>i) {
-				i = index-1;
-			}
-			dict.put(key, idx++);
-		}
-		
-		return answer = ans.stream().mapToInt(Integer::intValue).toArray();
+        int num = 1;
+        char c = 'A';
+        for(;num<=26;num++)
+        	hMap.put(String.valueOf(c++), num);
+        
+        ans.add(hMap.get(msg.substring(0,1)));
+        if(msg.length()>1)
+            hMap.put(msg.substring(0,2), num++);
+        
+
+        int pre = 1;
+        int end = msg.length();
+        while(pre<msg.length()) {
+        	String curr = msg.substring(pre, end);
+        	if(hMap.containsKey(curr)) {
+        		ans.add(hMap.get(curr));
+                if(end<msg.length())
+            		hMap.put(msg.substring(pre,end+1), num++);
+        		pre = end;
+        		end = msg.length();
+        	}else end--;
+        }
+        
+        answer = new int[ans.size()];
+        for(int i=0;i<ans.size();i++)
+        	answer[i] = ans.get(i);
+        
+        return answer;
     }
-    private static String makeStr(int start, String msg, HashMap<String, Integer> dict) {
-		StringBuilder sb = new StringBuilder();
-		
-		for(int i=start;i<msg.length();i++) {
-			sb.append(msg.charAt(i));
-			
-			String temp = sb.toString();
-			if(!dict.containsKey(temp)) {
-				ans.add(dict.get(msg.substring(start, i)));
-				if(i>=start+2)
-					index = i;
-				return temp;
-			}
-		}
-		ans.add(dict.get(sb.toString()));
-		index = msg.length();
-		return sb.toString();
-	}
 }
