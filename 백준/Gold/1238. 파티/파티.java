7b435c5c7bf0f555704f2062faf28632ba38c9c0
karam Choi <86296569+karamChoi2523@users.noreply.github.com>
2025-07-18 16:34:06 +0900

    [Gold III] Title: 파티, Time: 528 ms, Memory: 86996 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.java
+++ 백준/Gold/1238. 파티/파티.java
@@ -1,90 +1,83 @@
 import java.io.*;
 import java.util.*;
 
-
 public class Main {
-	static public class City implements Comparable<City>{
-		int end;
+	static class Edge implements Comparable<Edge>{
+		int to;
 		int cost;
 		
-		public City(int e, int c) {
-			end = e;
-			cost = c;
+		public Edge(int to, int cost) {
+			this.to = to;
+			this.cost = cost;
 		}
-
+		
 		@Override
-		public int compareTo(City o) {
+		public int compareTo(Edge o) {
+			if(this.cost == o.cost)
+				return this.to-o.to;
 			return this.cost-o.cost;
 		}
-		
-		
 	}
 	
-	static int n, m, x;
-	
+	static int N, M, X;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+		
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		n = Integer.valueOf(st.nextToken());
-		m = Integer.valueOf(st.nextToken());
-		x = Integer.valueOf(st.nextToken());
 
-		List<List<City>> adj = new ArrayList<>();
-		List<List<City>> reversedAdj = new ArrayList<>();
-		
-		for(int i=0;i<n+1;i++) {
-			adj.add(new ArrayList<>());
-			reversedAdj.add(new ArrayList<>());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		X = Integer.parseInt(st.nextToken());
+	
+		ArrayList<Edge>[] adj = new ArrayList[N+1];
+		for(int i=1;i<=N;i++) {
+			adj[i] = new ArrayList<>();
+			adj[i].add(new Edge(i, 0));
 		}
-		
-		for(int i=0;i<m;i++) {
+		for(int i=0;i<M;i++) {
 			st = new StringTokenizer(br.readLine());
 
-			int s = Integer.valueOf(st.nextToken());
-			int e = Integer.valueOf(st.nextToken());
-			int cost = Integer.valueOf(st.nextToken());
+			int start = Integer.parseInt(st.nextToken());
+			int end = Integer.parseInt(st.nextToken());
+			int T = Integer.parseInt(st.nextToken());
 			
-			adj.get(s).add(new City(e, cost));
-			reversedAdj.get(e).add(new City(s, cost));
+			adj[start].add(new Edge(end, T));
 		}
-		
-		int[] distX = dijkstra(adj);	//X에서 시작점으로
-		int[] dist = dijkstra(reversedAdj);	//시작점에서 X로
 		
 		int max = -1;
-		for(int i=1;i<n+1;i++) {
-			max = Math.max(max, dist[i]+distX[i]);
-		}
+		for(int i=1;i<=N;i++)
+			max = Math.max(max, dijkstra(adj, i, X)+dijkstra(adj, X, i));
 		
 		System.out.println(max);
 	}
-	private static int[] dijkstra(List<List<City>> adj) {
-		PriorityQueue<City> pq = new PriorityQueue<>();
-		pq.offer(new City(x,0));
-		
-		boolean[] check = new boolean[n+1];
-		int[] dist = new int[n+1];
+	static int dijkstra(ArrayList<Edge>[] adj, int start, int end) {
+		PriorityQueue<Edge> pq = new PriorityQueue<>();
+		pq.add(new Edge(start, 0));
+		boolean[] visited = new boolean[N+1];
+		int[] dist = new int[N+1];
 		Arrays.fill(dist, (int)1e9);
-		dist[x] = 0;
+		dist[start] = 0;
 		
+		//go
 		while(!pq.isEmpty()) {
-			City curr = pq.poll();
+			Edge curr = pq.poll();
 			
-			int end = curr.end;
+			if(curr.to==end) return dist[end];
 			
-			if(!check[end]) {
-				check[end] = true;
+			if(visited[curr.to]) continue;
+			
+			visited[curr.to] = true;
+			
+			for(Edge next : adj[curr.to]) {
+				int cost = curr.cost+next.cost;
 				
-				for(City city : adj.get(end)) {
-					if(!check[city.end] && dist[city.end] > dist[end]+city.cost) {
-						dist[city.end] = dist[end]+city.cost;
-						pq.add(new City(city.end, dist[city.end]));
-					}
-				}
+				if(dist[next.to]<=cost) continue;
+				
+				pq.add(new Edge(next.to, cost));
+				dist[next.to] = cost;
 			}
 		}
 		
-		return dist;
+		return dist[end];
 	}
 }
