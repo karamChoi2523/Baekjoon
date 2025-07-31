c713c76215a674e4f203e06a3e3648b742deffd8
karam Choi <86296569+karamChoi2523@users.noreply.github.com>
2025-07-31 17:38:02 +0900

    [Gold III] Title: 게리맨더링, Time: 100 ms, Memory: 13884 KB -BaekjoonHub

--- 백준/Gold/17471. 게리맨더링/게리맨더링.java
+++ 백준/Gold/17471. 게리맨더링/게리맨더링.java
@@ -1,53 +1,48 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 	static int N;
-	static ArrayList<Integer>[] adj;
 	static int[] person;
-	static int total;
-	static boolean[] visited;
+	static ArrayList<Integer>[] adj;
 	static int min;
+	static int total = 0;
+	static boolean[] visited;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+		
 		N = Integer.parseInt(br.readLine());
-		adj = new ArrayList[N+1];
-		for(int i=1;i<N+1;i++)
-			adj[i] = new ArrayList<>();
 		person = new int[N+1];
+		adj = new ArrayList[N+1];
+		
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int i=1;i<N+1;i++) {
+			adj[i] = new ArrayList<>();
 			person[i] = Integer.parseInt(st.nextToken());
 			total += person[i];
 		}
+		
 		for(int i=1;i<N+1;i++) {
 			st = new StringTokenizer(br.readLine());
 			int cnt = Integer.parseInt(st.nextToken());
-
-			while(st.hasMoreTokens()) {
-				int node = Integer.parseInt(st.nextToken());
-				adj[i].add(node);
+			
+			for(int j=0;j<cnt;j++) {
+				adj[i].add(Integer.parseInt(st.nextToken()));
 			}
 		}
-
+		
 		visited = new boolean[N+1];
 		min = total;
 		pick(1);
-
+		
 		System.out.println(min==total?-1:min);
-	}	
-	static void pick(int idx) {
+	}
+	public static void pick(int idx) {
 		if(idx==N+1) {
 			int cnt = 0;
-			for(int i=1;i<N+1;i++)
-				if(visited[i])
-					cnt++;
+			for(int i=1;i<N+1;i++) {
+				if(visited[i]) cnt++;
+			}
 			
 			if(cnt>0 && cnt<N)
 				if(checkGroup('A') && checkGroup('B'))
@@ -61,44 +56,42 @@ public class Main {
 		visited[idx] = false;
 		pick(idx+1);
 	}
-	
 	static boolean checkGroup(char c) {
-		ArrayList<Integer> list = new ArrayList<>();
-		
 		boolean target = false;
 		if(c=='A')
 			target = true;
 		
+		ArrayList<Integer> list = new ArrayList<>();
 		for(int i=1;i<N+1;i++)
 			if(visited[i]==target)
 				list.add(i);
 		
 		Queue<Integer> q = new LinkedList<>();
-		boolean[] visit = new boolean[N+1];
+		boolean[] check = new boolean[N+1];
 		q.add(list.get(0));
-		visit[list.get(0)] = true;
+		check[list.get(0)] = true;
 		
-		int idx = 0;
-		int cnt = 1;
+		int cnt=1;
 		while(!q.isEmpty()) {
 			int curr = q.poll();
 			
-			for(int e : adj[curr]) {
-				if(!visit[e] && list.contains(e)) {
-					visit[e] = true;
-					q.add(e);
+			for(int next : adj[curr]) {
+				if(!check[next] && list.contains(next)) {
+					check[next] = true;
+					q.add(next);
 					cnt++;
 				}
 			}
 		}
+		
 		return cnt==list.size();
 	}
 	static int calDiff() {
-		int areaA = 0;
-
+		int sum = 0;
 		for(int i=1;i<N+1;i++)
 			if(visited[i])
-				areaA+=person[i];
-		return Math.abs(total-areaA*2);
+				sum+=person[i];
+		
+		return Math.abs(total-sum*2);
 	}
 }
