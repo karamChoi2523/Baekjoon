d92660f1bbd94e873637f45688fab5e2da3d6189
karam Choi <86296569+karamChoi2523@users.noreply.github.com>
2025-08-07 16:06:58 +0900

    [Gold III] Title: 캐슬 디펜스, Time: 176 ms, Memory: 18504 KB -BaekjoonHub

--- 백준/Gold/17135. 캐슬 디펜스/캐슬 디펜스.java
+++ 백준/Gold/17135. 캐슬 디펜스/캐슬 디펜스.java
@@ -1,133 +1,126 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
 
 public class Main {
+	static class Pos{
+		int x, y;
+		
+		public Pos(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+		
+		@Override
+		public boolean equals(Object o) {
+			Pos p = (Pos)o;
+			return (this.x==p.x && this.y==p.y);
+		}
+		
+		@Override
+		public int hashCode() {
+			return 31*x+y;
+		}
+	}
+	static int N, M, D;
 	static int[][] board;
-	static int N,M,D;
-	static boolean[] visited;
+	static Set<Pos> attackPosSet;
+	static int row;
+	static int[] select;
 	static int max = 0;
-	static int answer = 0;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		D = Integer.parseInt(st.nextToken());
-
 		board = new int[N][M];
-
+		
 		for(int i=0;i<N;i++) {
-			String[] temp = br.readLine().split(" ");
-			for(int j=0;j<M;j++)
-				board[i][j] = Integer.parseInt(temp[j]);
-		}
-		if(checkBoard(board)) {
-			System.out.println(0);
-			return;
-		}
-
-		//궁수 배치 - 모든 경우의 수... pick
-		visited = new boolean[M];
-		pick(0,0);
-
-		System.out.println(max);
-	}
-	static void playGame(int[] arrowPos) {
-		int[][] copyBoard = new int[N][M];
-
-		for(int i=0;i<N;i++)
-			for(int j=0;j<M;j++)
-				copyBoard[i][j] = board[i][j];
-		//공격
-		answer = 0;
-		while(!checkBoard(copyBoard)) {
-			copyBoard = attack(copyBoard, arrowPos);
-			//이동 + 졌나?/이겼나?
-			move(copyBoard);
-		}
-		max = Math.max(max, answer);
-	}
-	private static int[][] attack(int[][] copyBoard, int[] arrowPos) {
-		//각 궁수와 가장 가까운 적 확인. 이 중 가장 왼쪽..
-		//궁수A와의 거리가 D이하이면서 가장 가까운 적 list... 이중 가장 왼쪽 애 ArrayList<int[]>
-
-		boolean[][] check = new boolean[N][M];	//동시공격. 같은 적 공격 가능
-		for(int i=0;i<3;i++) {	//궁수
-			int[] target = kill(copyBoard, arrowPos[i]);
-
-			if(target != null)
-				check[target[0]][target[1]] = true;
-		}
-
-		for(int i=0;i<N;i++)
-			for(int j=0;j<M;j++)
-				if(check[i][j]) {
-					copyBoard[i][j]=0;
-					answer++;
-				}
-
-		return copyBoard;
-	}
-	private static int[] kill(int[][] copyBoard, int arrow) {
-		int minDist = D;
-		int minRow = N, minCol = M;
-
-		for (int i = 0; i < N; i++)
-			for (int j = 0; j < M; j++)
-				if (copyBoard[i][j] == 1) {
-					int d = calDist(N, arrow, i, j);
-					if (d < minDist) {
-						minDist = d;
-						minRow = i;
-						minCol = j;
-					} else if (d == minDist && j < minCol) {
-						minRow = i;
-						minCol = j;
-					}
-				}
-
-		return minRow != N ? new int[]{minRow, minCol} : null;
-	}
-
-	static void pick(int idx, int picked) {
-		if(picked==3) {
-			int[] arrowPos = new int[3];
-			int index = 0;
-			for(int i=0;i<M;i++)
-				if(visited[i])
-					arrowPos[index++] = i;
-
-			playGame(arrowPos);
-			return;
-		}
-
-		for(int i=idx;i<M;i++) {
-			if(!visited[i]) {
-				visited[i] = true;
-				pick(i+1, picked+1);
-				visited[i] = false;
+			st = new StringTokenizer(br.readLine());
+			for(int j=0;j<M;j++) {
+				board[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
+		
+		//궁수 배치
+		select = new int[3];
+		combination(0,0);
+		
+		System.out.println(max);
 	}
-
+	static int[][] deepCopy(){
+		int[][] copy = new int[N][M];
+		for(int i=0;i<N;i++)
+			System.arraycopy(board[i], 0, copy[i], 0, M);
+		return copy;
+	}
+	static void attack(int col, int[][] copy) {
+		int x=-1, y=-1;	//적 위치
+		int minD = D;
+		
+		int preCol = M;
+		for(int i=row-1;i>=0;i--)
+			for(int j=0;j<M;j++) {
+				if(copy[i][j]==1) {
+					int d = calDist(row, col, i, j);
+					if(d<minD) {
+						minD = d;
+						preCol = j;
+						x = i;
+						y = j;
+					}else if(d==minD && j<preCol) {
+						preCol = j;
+						x = i;
+						y = j;
+					}
+				}
+			}
+		
+		if(x==-1 && y==-1) return;
+		attackPosSet.add(new Pos(x, y));
+	}
+	static void combination(int idx, int k) {
+		if(k==3) {
+			row = N; //시작 궁수 행
+			int total = 0;
+			int[][] copy = deepCopy();
+			while(true) {
+				if(row<=0 || gameOver(copy)) break;
+				attackPosSet = new HashSet<>();	//각 턴의 공격하는 적 위치
+				for(int i=0;i<k;i++)
+					attack(select[i],copy); //궁수가 [row][select[i]]에서 공격
+				
+				total+=attackPosSet.size();
+				for(Pos pos : attackPosSet) {
+					copy[pos.x][pos.y] = 0;
+				}
+				
+				row--;
+			}
+			max = Math.max(max, total);
+			return;
+		}
+		
+		if(idx==M) return;
+		
+		select[k] = idx;
+		combination(idx+1, k+1);
+		combination(idx+1, k);
+	}
 	static int calDist(int r1, int c1, int r2, int c2) {
 		return Math.abs(r1-r2)+Math.abs(c1-c2);
 	}
-
-	static boolean checkBoard(int[][] copyBoard) {
-		for(int i=0;i<N;i++)
+	static boolean gameOver(int[][] copy) {
+		for(int i=row-1;i>=0;i--)
 			for(int j=0;j<M;j++)
-				if(copyBoard[i][j]==1)
-					return false;
-
+				if(copy[i][j]==1) return false;
+		
 		return true;
-	}
-	static void move(int[][] copyBoard) {
-		for(int i=N-1;i>0;i--)
-			copyBoard[i] = Arrays.copyOf(copyBoard[i - 1], M);
-
-		Arrays.fill(copyBoard[0],0);
 	}
 }
