9af86cb6cee89236fa96f3b5ba1980e7377339a9
karam Choi <86296569+karamChoi2523@users.noreply.github.com>
2025-08-08 21:42:20 +0900

    [Gold II] Title: 색종이 붙이기, Time: 140 ms, Memory: 16896 KB -BaekjoonHub

--- 백준/Gold/17136. 색종이 붙이기/색종이 붙이기.java
+++ 백준/Gold/17136. 색종이 붙이기/색종이 붙이기.java
@@ -3,64 +3,73 @@ import java.util.*;
 
 public class Main {
 	static int[] papers = {5,5,5,5,5};
-	//static int[][] board = new int[10][10];
+	static int[][] board = new int[10][10];
 	static int min = Integer.MAX_VALUE;
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int[][] board = new int[10][10];
+		
 		for(int i=0;i<10;i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			
 			for(int j=0;j<10;j++)
 				board[i][j] = Integer.parseInt(st.nextToken());
 		}
 		
-		dfs(board, 0,0,0);
-		
+		dfs(0,0,0);
+//		
+//		int cnt = 0;
+//		for(int i=0;i<10;i++)
+//			for(int j=0;j<10;j++) {
+//				if(board[i][j]==1) {
+//					for(int k=5;k>=1;k--)
+//						if(papers[k-1]>0 && check(i, j, k)) {
+//							toggle(i, j, k);
+//							papers[k-1]-=1;
+//							cnt++;
+//						}
+//				}
+//			}
+	
 		System.out.println(min==Integer.MAX_VALUE?-1:min);
 	}
-	static void dfs(int[][] board, int x, int y, int cnt) {
-		if(x==10) {
-			for(int i=0;i<10;i++)
-				for(int j=0;j<10;j++)
-					if(board[i][j]==1)
-						return;
+	static void dfs(int x, int y, int cnt) {
+		if(y>=10) {
+			x++;
+			y=0;
+		}
+		
+		if(x>=10) {
 			min = Math.min(min, cnt);
 			return;
 		}
 		
-		if(y==10) {
-			dfs(board, x+1, 0, cnt);
+		if(board[x][y]==0) {
+			dfs(x, y+1, cnt);
 			return;
 		}
 		
-		if(board[x][y]==1) {
-			for(int i=5;i>=1;i--) {
-				if(papers[i-1]>0 && check(board, i, x, y)) {
-					attach(board, i, x, y, 0);
-					papers[i-1]--;
-					dfs(board, x, y+i, cnt+1);
-					attach(board, i, x, y, 1);
-					papers[i-1]++;
-				}
+		for(int i=5;i>0;i--) {
+			if(papers[i-1]>0 && check(x, y, i)) {
+				toggle(x, y, i);
+				papers[i-1]--;
+				dfs(x, y+i, cnt+1);
+				toggle(x, y, i);
+				papers[i-1]++;
 			}
-		}else
-			dfs(board, x, y+1, cnt);
+		}
 	}
-	static boolean check(int[][] board, int size, int x, int y) {
-		if(x+size>10 || y+size>10)
-			return false;
+	static boolean check(int x, int y, int size) {
+		if(x+size>10 || y+size>10) return false;
 		
-		for(int i=0;i<size;i++)
-			for(int j=0;j<size;j++)
-				if(board[x+i][y+j]!=1)
+		for(int i=x;i<x+size;i++)
+			for(int j=y;j<y+size;j++)
+				if(board[i][j]!=1)
 					return false;
 		
 		return true;
 	}
-	static void attach(int[][] board, int size, int x, int y, int state) {
-		for(int i=0;i<size;i++)
-			for(int j=0;j<size;j++)
-				board[x+i][y+j] = state;
+	static void toggle(int x, int y, int size) {
+		for(int i=x;i<x+size;i++)
+			for(int j=y;j<y+size;j++)
+				board[i][j] = board[i][j]==0?1:0;
 	}
 }
