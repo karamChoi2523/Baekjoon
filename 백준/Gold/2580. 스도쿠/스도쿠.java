51888922ab27048e32576b27bc5e241e206c323c
karam Choi <86296569+karamChoi2523@users.noreply.github.com>
2025-08-14 01:22:26 +0900

    [Gold IV] Title: 스도쿠, Time: 200 ms, Memory: 15244 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.java
+++ 백준/Gold/2580. 스도쿠/스도쿠.java
@@ -3,59 +3,51 @@ import java.util.*;
 
 public class Main {
 	static int[][] board = new int[9][9];
-	static ArrayList<int[]> empty = new ArrayList<>();
 	static boolean[][] rows = new boolean[9][10];
 	static boolean[][] cols = new boolean[9][10];
 	static boolean[][] boxes = new boolean[9][10];
-	static boolean solved = false;
-
+	static ArrayList<int[]> posList;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-
+		
+		posList = new ArrayList<>();
 		for(int i=0;i<9;i++) {
 			st = new StringTokenizer(br.readLine());
-
 			for(int j=0;j<9;j++) {
 				int temp = Integer.parseInt(st.nextToken());
 				board[i][j] = temp;
 				if(temp==0)
-					empty.add(new int[] {i,j});
+					posList.add(new int[] {i,j});
 				else {
 					rows[i][temp] = true;
 					cols[j][temp] = true;
-					boxes[calIndex(i,j)][temp] = true;
+					boxes[(i/3)*3+j/3][temp] = true;
 				}
 			}
 		}
-
 		dfs(0);
 	}
 	static void dfs(int idx) {
-		if(idx == empty.size()) {
+		if(idx==posList.size()) {
 			print();
-			solved = true;
-			return;
+			System.exit(0);
 		}
-
-		int x = empty.get(idx)[0];
-		int y = empty.get(idx)[1];
-		int checkBox = calIndex(x, y);
-
+		
+		int[] curr = posList.get(idx);
+		int x = curr[0];
+		int y = curr[1];
+		
 		for(int i=1;i<10;i++) {
-			if(rows[x][i] || cols[y][i] || boxes[checkBox][i]) continue;
+			if(rows[x][i] || cols[y][i] || boxes[(x/3)*3+y/3][i]) continue;
 			
 			board[x][y] = i;
-			rows[x][i] = cols[y][i] = boxes[checkBox][i] = true;
-			
+			rows[x][i] = cols[y][i] = boxes[(x/3)*3+y/3][i] = true;
 			dfs(idx+1);
-			if(solved) return;
+			rows[x][i] = cols[y][i] = boxes[(x/3)*3+y/3][i] = false;
 			board[x][y] = 0;
-			rows[x][i] = cols[y][i] = boxes[checkBox][i] = false;
 		}
-	}
-	static int calIndex(int x, int y) {
-		return (x/3)*3 + y/3;
+		
 	}
 	static void print() {
 		StringBuilder sb = new StringBuilder();
