5a700f45867d2c60d95d21f41c2c1d558ff3c068
karam Choi <86296569+karamChoi2523@users.noreply.github.com>
2025-08-15 21:29:35 +0900

    [Gold III] Title: 줄 세우기, Time: 384 ms, Memory: 50576 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -1,58 +1,52 @@
 import java.io.*;
 import java.util.*;
 
+import com.sun.corba.se.impl.orbutil.graph.Graph;
 
 public class Main {
-	static int n,m;
+	static int N, M;
 	static ArrayList<Integer>[] graph;
 	static int[] indegree;
-	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		StringTokenizer st = new StringTokenizer(br.readLine());
-
-		n = Integer.valueOf(st.nextToken());
-		m = Integer.valueOf(st.nextToken());
+		StringTokenizer st;
 		
-		graph = new ArrayList[n+1];
-		indegree = new int[n+1];
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		indegree = new int[N+1];
+		graph = new ArrayList[N+1];
 		
-		for(int i=0;i<n+1;i++)
+		for(int i=1;i<N+1;i++)
 			graph[i] = new ArrayList<>();
-	
-		for(int i=0;i<m;i++) {
-			st = new StringTokenizer(br.readLine());
-
-			int a = Integer.valueOf(st.nextToken());
-			int b = Integer.valueOf(st.nextToken());
 		
-			graph[a].add(b);	//a가 먼저
-			indegree[b]++;
+		for(int i=0;i<M;i++) {
+			st = new StringTokenizer(br.readLine());
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			
+			graph[A].add(B);
+			indegree[B]++;
 		}
 		
-		bfs();
-	}
-
-	private static void bfs() {
+		StringBuilder sb = new StringBuilder();
 		Queue<Integer> q = new LinkedList<>();
-		for(int i=1;i<n+1;i++) {
+		
+		for(int i=1;i<N+1;i++)
 			if(indegree[i]==0)
 				q.add(i);
-		}
-				
+		
 		while(!q.isEmpty()) {
 			int curr = q.poll();
 			
-			System.out.print(curr+" ");
+			sb.append(curr+" ");
 			
-			for(int i=0;i<graph[curr].size();i++) {
-				int t = graph[curr].get(i);
-				
-				indegree[t]--;
-				if(indegree[t]==0)
-					q.add(t);
+			for(int next : graph[curr]) {
+				indegree[next]--;
+				if(indegree[next]==0)
+					q.add(next);
 			}
 		}
+		System.out.println(sb);
 	}
 }
