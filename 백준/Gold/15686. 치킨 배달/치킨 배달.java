4bdf62275397ee1bb7613bec953f56ce0d416135
karam Choi <86296569+karamChoi2523@users.noreply.github.com>
2025-08-16 19:37:44 +0900

    [Gold V] Title: 치킨 배달, Time: 136 ms, Memory: 14188 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -1,76 +1,81 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-	static int[][] board;
-	static int n, m;
-	static boolean[] open;
-	static ArrayList<int[]> houses, posList;
-	static int answer = Integer.MAX_VALUE;
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System .in));
-
-		StringTokenizer st = new StringTokenizer(br.readLine());
-
-		n = Integer.valueOf(st.nextToken());
-		m = Integer.valueOf(st.nextToken());
-
-		board = new int[n+1][n+1];
+	static class Chicken{
+		int x, y;
 		
-		houses = new ArrayList<>();
-		posList = new ArrayList<>();	//치킨집
-		for(int i=1;i<n+1;i++) {
+		public Chicken(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
+	static int N, M;
+	static int[][] board;
+	static ArrayList<Chicken> chickens = new ArrayList<>();
+	static int[] select;
+	static int min = Integer.MAX_VALUE;
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		select = new int[M];
+		board = new int[N+1][N+1];
+		
+		for(int i=1;i<N+1;i++) {
 			st = new StringTokenizer(br.readLine());
-			for(int j=1;j<n+1;j++) {
-				board[i][j] = Integer.valueOf(st.nextToken());
-				
-				if(board[i][j]==1)
-					houses.add(new int[] {i, j});
-				else if(board[i][j]==2)
-					posList.add(new int[] {i, j});
-				
+			
+			for(int j=1;j<N+1;j++) {
+				board[i][j] = Integer.parseInt(st.nextToken());
+				if(board[i][j]==2) {
+					chickens.add(new Chicken(i, j));
+				}
 			}
 		}
 		
-		//1<= 집 <=2n
-		//m<= 치킨집 <=13
-		
-		//폐업x 치킨집 최대 m개. 치킨 거리의 최솟값.
-
-		open = new boolean[posList.size()];
-		
-		//폐업
-		//m개를 pick
-		//가장 최솟값을 갖는 경우를 구한다
-		//백트래킹
-		dfs(0,0);
-		System.out.println(answer);
+		if(chickens.size()>M)
+			combination(0,0);
+		else {
+			Arrays.fill(select, -1);
+			for(int i=0;i<chickens.size();i++)
+				select[i] = i;
+			min = cal();
+		}
+		System.out.println(min);
 	}
-	private static void dfs(int start, int cnt) {
-		if(cnt == m) {
-			int sum = 0;
-			
-			for(int[] house : houses) {
-				int min = Integer.MAX_VALUE;
-				
-				for(int i=0;i<posList.size();i++)
-					if(open[i]) {
-						int temp = Math.abs(house[0]-posList.get(i)[0])+Math.abs(house[1]-posList.get(i)[1]);
-						min = Math.min(min, temp);
+	static int cal() {
+		int total = 0;
+		for(int i=1;i<N+1;i++)
+			for(int j=1;j<N+1;j++) {
+				if(board[i][j]==1) {
+					int minD = Integer.MAX_VALUE;
+					for(int k=0;k<M;k++) {
+						if(select[k]==-1) break;
+						minD = Math.min(minD, calDist(chickens.get(select[k]),i,j));
 					}
-				sum += min;
+					total+=minD;
+				}
 			}
-			answer = Math.min(answer, sum);
+		
+		return total;
+	}
+	static void combination(int idx, int k) {
+		if(k==M) {
+			//System.out.println(Arrays.toString(select));
+			min = Math.min(min, cal());
 			return;
 		}
 		
-		//백트래킹 - m개 pick
-		for(int i=start;i<posList.size();i++) {
-			open[i] = true;
-			dfs(i+1, cnt+1);
-			open[i] = false;
-		}
-	}	
+		if(idx==chickens.size()) return;
+		
+		select[k] = idx;
+		combination(idx+1, k+1);
+		combination(idx+1, k);
+	}
+	
+	static int calDist(Chicken p, int r2, int c2) {
+		return Math.abs(p.x-r2)+Math.abs(p.y-c2);
+	}
 }
