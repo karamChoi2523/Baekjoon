38ac6a35239b78fa97aa6e50206d9e448f996385
karam Choi <86296569+karamChoi2523@users.noreply.github.com>
2025-08-19 20:14:07 +0900

    [Unrated] Title: [모의 SW 역량테스트] 무선 충전, Time: 106 ms, Memory: 29,412 KB -BaekjoonHub

--- SWEA/Unrated/5644. ［모의 SW 역량테스트］ 무선 충전/［모의 SW 역량테스트］ 무선 충전.java
+++ SWEA/Unrated/5644. ［모의 SW 역량테스트］ 무선 충전/［모의 SW 역량테스트］ 무선 충전.java
@@ -19,50 +19,44 @@ public class Solution {
 			return o.performance-this.performance;
 		}
 	}
-	static int max = 0;
-	static int[][] board;
 	static int M, A;
+	static int[][] board;
 	static int[] aList;
 	static int[] bList;
 	static ArrayList<BC> bcList;
-	
-	static int[] dx = {0,0,1,0,-1};
-	static int[] dy = {0,-1,0,1,0};
-	
-	static int ax = 1, ay = 1;
-	static int bx = 10, by = 10;
-
+	static int ax,ay,bx,by;
+	static int[] dx = {0,0,1,0,-1};	//열
+	static int[] dy = {0,-1,0,1,0};	//행
+	static int max;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		
 		int T = Integer.parseInt(br.readLine());
 		for(int tc=1;tc<=T;tc++) {
-			board = new int[11][11];
-			max = 0;
-			
 			st = new StringTokenizer(br.readLine());
-			M = Integer.parseInt(st.nextToken()); //총 이동 시간
-			A = Integer.parseInt(st.nextToken()); //BC 개수
-			
-			aList= new int[M+1];
+			M = Integer.parseInt(st.nextToken());
+			A = Integer.parseInt(st.nextToken());
+			board = new int[11][11];
+			aList = new int[M+1];
+			bList = new int[M+1];
+			bcList = new ArrayList<>();
+
 			st = new StringTokenizer(br.readLine());
 			for(int i=1;i<M+1;i++)
 				aList[i] = Integer.parseInt(st.nextToken());
 
-			bList= new int[M+1];
 			st = new StringTokenizer(br.readLine());
 			for(int i=1;i<M+1;i++)
 				bList[i] = Integer.parseInt(st.nextToken());
 			
-			bcList = new ArrayList<>();
 			for(int i=0;i<A;i++) {
 				st = new StringTokenizer(br.readLine());
 				int x = Integer.parseInt(st.nextToken());
 				int y = Integer.parseInt(st.nextToken());
 				int c = Integer.parseInt(st.nextToken());
 				int p = Integer.parseInt(st.nextToken());
-				
+			
 				bcList.add(new BC(x,y,c,p));
 			}
 			
@@ -72,10 +66,10 @@ public class Solution {
 			ay = 1;
 			bx = 10;
 			by = 10;
-						
+			max = 0;
 			for(int time=0;time<M+1;time++) {
 				move(time);
-				coverage();
+				calCoverage();
 			}
 			
 			System.out.printf("#%d %d\n",tc,max);
@@ -90,52 +84,42 @@ public class Solution {
 			this.p = p;
 		}
 	}
-	static void coverage() {
-		ArrayList<State> sa = new ArrayList<>();
-		ArrayList<State> sb = new ArrayList<>();
-		
+	static void calCoverage() {
+		ArrayList<State> ca = new ArrayList<>();
+		ArrayList<State> cb = new ArrayList<>();
 		
 		for(int i=0;i<bcList.size();i++) {
 			BC bc = bcList.get(i);
-			if(isInCoverage(bc, ax, ay))
-				sa.add(new State(i, bc.performance));
+			if(isInCoverage(bc,ax, ay))
+				ca.add(new State(i, bc.performance));
 			if(isInCoverage(bc, bx, by))
-				sb.add(new State(i, bc.performance));
+				cb.add(new State(i,bc.performance));
 		}
-		//1. 반반 먹기
-		//2. a만 먹기
-		//3. b만 먹기
 		
 		int cmax = 0;
-		
-		if(sa.size()>0 && sb.size()>0)
-			for(int i=0;i<sa.size();i++) {
-				State ca = sa.get(i);
-				for(int j=0;j<sb.size();j++) {
-					State cb = sb.get(j);
-					
-					if(ca.index==cb.index)
-						cmax = Math.max(ca.p, cmax);
-					else
-						cmax = Math.max(ca.p+cb.p, cmax);
-				}
+		for(int i=0;i<ca.size();i++) {
+			State pa = ca.get(i);
+			for(int j=0;j<cb.size();j++) {
+				State pb = cb.get(j);
+				
+				if(pa.index==pb.index) {
+					cmax = Math.max(cmax, pa.p);
+				}else
+					cmax = Math.max(cmax, pa.p+pb.p);
 			}
-		for(int i=0;i<sa.size();i++)
-			cmax = Math.max(cmax, sa.get(i).p);
+		}
+		if(ca.size()>0) cmax = Math.max(cmax, ca.get(0).p);
+		if(cb.size()>0) cmax = Math.max(cmax, cb.get(0).p);
 		
-		for(int i=0;i<sb.size();i++)
-			cmax = Math.max(cmax, sb.get(i).p);
-		max+=cmax;
+		max += cmax;
 	}
 	static void move(int time) {
-		int dir = aList[time];
-		ax+=dx[dir];
-		ay+=dy[dir];
-		dir = bList[time];
-		bx+=dx[dir];
-		by+=dy[dir];
+		ax += dx[aList[time]];
+		ay += dy[aList[time]];
+		bx += dx[bList[time]];
+		by += dy[bList[time]];
 	}
 	static boolean isInCoverage(BC bc, int x, int y) {
-		return (Math.abs(bc.x-x)+Math.abs(bc.y-y)) <= bc.coverage;
+		return Math.abs(bc.x-x)+Math.abs(bc.y-y)<=bc.coverage;
 	}
 }
