2e7f00d8d34dcd002acbd92a73d62d4417c84475
karam Choi <86296569+karamChoi2523@users.noreply.github.com>
2025-08-21 10:08:11 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 604 ms, Memory: 162412 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -2,30 +2,10 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static class Pos{
-		int x, y;
-		
-		public Pos(int x, int y) {
-			this.x = x;
-			this.y = y;
-		}
-		
-		@Override
-		public boolean equals(Object obj) {
-			Pos p = (Pos) obj;
-			return this.x==p.x && this.y==p.y;
-		}
-		
-		@Override
-		public int hashCode() {
-			return Objects.hash(x,y);
-		}
-	}
-	static int N, M;
+	static int N,M;
+	static int[][] board;
 	static int[] dx = {-1,1,0,0};
 	static int[] dy = {0,0,-1,1};
-	static int[][] board;
-	static int min = Integer.MAX_VALUE;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
@@ -33,6 +13,7 @@ public class Main {
 		st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
+		
 		board = new int[N][M];
 		for(int i=0;i<N;i++) {
 			char[] temp = br.readLine().toCharArray();
@@ -40,51 +21,47 @@ public class Main {
 				board[i][j] = temp[j]-'0';
 		}
 		
-		bfs();
-		
-		System.out.println(min==Integer.MAX_VALUE?-1:min);
+		System.out.println(bfs());
 	}
+	static int min = Integer.MAX_VALUE;
 	static int bfs() {
 		Queue<int[]> q = new LinkedList<>();
-		q.add(new int[] {0,0,1,0});
-		//해당 위치에 여러 번 갈 수 있다면 3차원 배열이 필요
-		//bfs는 먼저 온 게 최단경로
+		q.add(new int[] {0,0,0,1});	//x,y,did,len
 		boolean[][][] visited = new boolean[N][M][2];
 		visited[0][0][0] = true;
 		
 		while(!q.isEmpty()) {
-			int[] curr = q.poll();
+			int size = q.size();
 			
-			int cx = curr[0];
-			int cy = curr[1];
-			int cnt = curr[2];
-			int did = curr[3];
-			
-			if(cx==N-1 && cy==M-1) {
-				min = Math.min(min, cnt);
-				break;
-			}
-			
-			for(int d=0;d<4;d++) {
-				int nx = cx+dx[d];
-				int ny = cy+dy[d];
+			for(int step=0;step<size;step++) {
+				int[] curr = q.poll();
+				int cx = curr[0];
+				int cy = curr[1];
+				int did = curr[2];
+				int len = curr[3];
 				
-				if(checkNext(nx, ny) &&!visited[nx][ny][did]) {
+				if(cx==N-1 && cy==M-1) {
+					return len;
+				}
+				
+				for(int d=0;d<4;d++) {
+					int nx = cx+dx[d];
+					int ny = cy+dy[d];
+					
+					if(nx<0 || nx>=N || ny<0 || ny>=M) continue;
+					//도착 확인을 여기에서 하면 큐에 못 들어가는 경우를 답으로 반환할 수도 있어서 cx cy에 대해 체크해야 함!!!
+					if(visited[nx][ny][did]) continue;
+					
 					if(board[nx][ny]==1 && did==0) {
+						q.add(new int[] {nx, ny, 1, len+1});
 						visited[nx][ny][1] = true;
-						q.add(new int[] {nx, ny,cnt+1, 1});
-					}else if(board[nx][ny]!=1){
+					} else if(board[nx][ny]==0) {
+						q.add(new int[] {nx, ny, did, len+1});
 						visited[nx][ny][did] = true;
-						q.add(new int[] {nx, ny,cnt+1,did});
 					}
 				}
 			}
 		}
 		return -1;
-	}
-	static boolean checkNext(int x, int y) {
-		if(x<0 || x>=N || y<0 || y>=M) return false;
-
-		return true;
 	}
 }
