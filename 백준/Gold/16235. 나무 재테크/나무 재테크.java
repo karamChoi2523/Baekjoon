728f470f7b7092aa11ea7bce2c482a7776daa93b
karam Choi <86296569+karamChoi2523@users.noreply.github.com>
2025-08-23 00:16:33 +0900

    [Gold III] Title: 나무 재테크, Time: 108 ms, Memory: 16120 KB -BaekjoonHub

--- 백준/Gold/16235. 나무 재테크/나무 재테크.java
+++ 백준/Gold/16235. 나무 재테크/나무 재테크.java
@@ -4,40 +4,140 @@ import java.util.*;
 public class Main {
 	static int[] dx = {-1,-1,-1,0,0,1,1,1};
 	static int[] dy = {-1,0,1,-1,1,-1,0,1};
-	static class Point{
-		int x, y;
-		int power;
-		//ArrayList<Tree> trees;
-		PriorityQueue<Integer> trees;
-		
-		Point(int x, int y){
-			this.x = x;
-			this.y = y;
-			power = 5;
-			trees = new PriorityQueue<>();
+	static class Node{
+		int age, count;
+		Node next, prev;
+
+		Node(int age, int count){
+			this.age = age;
+			this.count = count;
+		}
+	}
+	static class Tree{
+		Node head;
+
+		Tree(){
+			head = new Node(0,0);
+		}
+
+		void addFirst(Node n) {
+			n.prev = head;
+			if(head.next!=null) {
+				n.next = head.next;
+				head.next.prev = n;
+			}
+			head.next = n;
+		}
+
+		void remove(Node n) {
+			n.prev.next = null;
 		}
 	}
 	static int N,M,K;
-	static Point[][] board;
+	static Tree[][] trees;
 	static int[][] A;
-	static int cnt;
+	static int[][] soil;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
 		
+		initialize(br);
+
+		while(K-->0) {
+			//봄 : 나이만큼 양분을 먹고 age+1(어린 나무부터)
+			for(int i=0;i<N;i++)
+				for(int j=0;j<N;j++) {
+					Node dead = null;
+					boolean alive = true;
+					Node curr = trees[i][j].head.next;
+					while(curr!=null) {
+						if(alive) {
+							int need = curr.age * curr.count;
+							if(soil[i][j]>=need) {
+								soil[i][j]-=need;
+								curr.age++;
+							}else {
+								int aliveCnt = soil[i][j]/curr.age;
+								soil[i][j] -= curr.age * aliveCnt;
+								soil[i][j] += (curr.count-aliveCnt)*(curr.age/2);
+								
+								if(aliveCnt > 0) {
+									curr.count = aliveCnt;
+									curr.age++;
+									dead = curr.next;
+								}else
+									dead = curr;
+								alive = false;
+							}
+						}else
+							soil[i][j] += curr.count*(curr.age/2);
+						curr = curr.next;
+					}
+					
+					if(dead != null)
+						trees[i][j].remove(dead);
+				}
+
+			//가을 : 번식. 나이가 5배수인 나무만. 인접한 8개 칸에 나이가 1인 나무들 추가
+			for(int i=0;i<N;i++)
+				for(int j=0;j<N;j++) {
+					soil[i][j] += A[i][j];
+					Node curr = trees[i][j].head.next;
+					while(curr!=null) {
+						if(curr.age%5==0) {
+							for(int d=0;d<8;d++) {
+								int nx = i+dx[d];
+								int ny = j+dy[d];
+								if(nx<0 || nx>=N || ny<0 || ny>=N) continue;
+								addNode(nx, ny, curr.count);
+							}
+						}
+						curr = curr.next;
+					}
+				}
+		}
+
+		System.out.println(count());
+	}
+	static int count() {
+		int total = 0;
+		for(int i=0;i<N;i++)
+			for(int j=0;j<N;j++) {
+				Node curr = trees[i][j].head.next;
+				while(curr!=null) {
+					total += curr.count;
+					curr = curr.next;
+				}
+			}
+		
+		return total;
+	}
+	static void addNode(int x, int y, int cnt) {
+		Node existing = trees[x][y].head.next;
+		if(existing!=null && existing.age==1)
+			existing.count += cnt;
+		else
+			trees[x][y].addFirst(new Node(1,cnt));
+	}
+	static void initialize(BufferedReader br) throws IOException {
+		StringTokenizer st;
+
 		st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(st.nextToken());
-		
-		board = new Point[N][N];
+
+		trees = new Tree[N][N];
 		A = new int[N][N];
-		
+		soil = new int[N][N];
+
+		for (int i=0;i<N;i++)
+            Arrays.fill(soil[i], 5);
+        
 		for(int i=0;i<N;i++) {
 			st = new StringTokenizer(br.readLine());
 			for(int j=0;j<N;j++) {
 				A[i][j] = Integer.parseInt(st.nextToken());
-				board[i][j] = new Point(i,j);
+				trees[i][j] = new Tree();
 			}
 		}
 		for(int i=0;i<M;i++) {
@@ -45,64 +145,9 @@ public class Main {
 			int x = Integer.parseInt(st.nextToken())-1;
 			int y = Integer.parseInt(st.nextToken())-1;
 			int z = Integer.parseInt(st.nextToken());
-			
-			board[x][y].trees.add(z);
-		}
-		
-		cnt = M;
-		for(int time=0;time<K;time++) {
-			//봄 : 나이만큼 양분을 먹고 age+1(어린 나무부터)
-			for(int i=0;i<N;i++)
-				for(int j=0;j<N;j++) {
-					PriorityQueue<Integer> ts = board[i][j].trees;
-					ArrayList<Integer> alive = new ArrayList<>();
-					//ArrayList<Integer> dead = new ArrayList<>();
-					int dead = 0;
-					while(!ts.isEmpty()) {
-						int curr = ts.poll();
-						if(board[i][j].power>=curr) {
-							board[i][j].power-=curr;
-							curr++;
-							alive.add(curr);
-						}else {
-							cnt--;
-							dead += curr/2;
-							//dead.add(curr);
-						}
-					}
-					
-					for(int t : alive) ts.add(t);
-					//여름 : 봄에 죽은 나무 -> 양분 (age/2)
-					board[i][j].power += dead;
-				}
 
-			//가을 : 번식. 나이가 5배수인 나무만. 인접한 8개 칸에 나이가 1인 나무들 추가
-			for(int i=0;i<N;i++)
-				for(int j=0;j<N;j++) {
-					//겨울 : 양분 추가. 각 칸에 +A[r][c]
-					board[i][j].power += A[i][j];
-					PriorityQueue<Integer> ts = board[i][j].trees;
-					ArrayList<Integer> tList = new ArrayList<>();
-					while(!ts.isEmpty()) {
-						int curr = ts.poll();
-						if(curr%5==0)
-							add(i,j);
-						tList.add(curr);
-					}
-					for(int t : tList) ts.add(t);
-				}
+			trees[x][y].addFirst(new Node(z,1));
 		}
-		
-		System.out.println(cnt);
-	}
-	static void add(int x, int y) {
-		for(int d=0;d<8;d++) {
-			int nx = x+dx[d];
-			int ny = y+dy[d];
-			
-			if(nx<0 || nx>=N || ny<0 || ny>=N) continue;
-			board[nx][ny].trees.offer(1);
-			cnt++;
-		}
+
 	}
 }
