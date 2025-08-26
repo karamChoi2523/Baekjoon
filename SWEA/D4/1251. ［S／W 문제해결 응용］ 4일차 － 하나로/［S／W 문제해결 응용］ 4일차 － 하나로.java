b9984a3a35fdca08cebf4cbd28ee082af69d9807
karam Choi <86296569+karamChoi2523@users.noreply.github.com>
2025-08-26 16:52:19 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 하나로, Time: 1,406 ms, Memory: 97,152 KB -BaekjoonHub

--- SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
+++ SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
@@ -1,82 +1,100 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
+
 
 public class Solution {
+	static int N;
+	static long[] X;
+	static long[] Y;
+	static double E;
+	static PriorityQueue<Edge> pq;
 	static int[] parents;
-	static class Edge implements Comparable<Edge>{
-		int from, to;
-		long cost;
-		
-		public Edge(int from, int to, long cost) {
-			this.from = from;
-			this.to = to;
-			this.cost = cost;
-		}
-
-		@Override
-		public int compareTo(Edge o) {
-			return Long.compare(this.cost, o.cost);
-		}
-	}
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int T = Integer.parseInt(br.readLine());
-		for(int t=1;t<=10;t++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int N = Integer.parseInt(st.nextToken());
-			
-			long[] X = new long[N];
-			long[] Y = new long[N];
 
-			st = new StringTokenizer(br.readLine());
-			for(int i=0;i<N;i++)
-				X[i] = Integer.parseInt(st.nextToken());
-			st = new StringTokenizer(br.readLine());
-			for(int i=0;i<N;i++)
-				Y[i] = Integer.parseInt(st.nextToken());
+		int T = Integer.parseInt(br.readLine().trim());
+		//int T = 10;
+		for(int tc=1;tc<=T;tc++) {
+			initialize(br);
 			
-			double E = Double.parseDouble(br.readLine());
+			makeEdges();
 			
-			parents = new int[N];
-			for(int i=0;i<N;i++)
-				parents[i] = i;
-			
-			PriorityQueue<Edge> pq = new PriorityQueue<>();
-			for(int i=0;i<N;i++)
-				for(int j=i+1;j<N;j++) {
-					long L = (X[i]-X[j])*(X[i]-X[j])+(Y[i]-Y[j])*(Y[i]-Y[j]);
-					pq.add(new Edge(i,j,L));
-				}
-			
-			long ans = 0;
-			int cnt=0;
+			double sum = 0;
 			while(!pq.isEmpty()) {
-				Edge edge = pq.poll();
-				if(union(edge.from, edge.to))
-					continue;
-				ans += edge.cost;
-				if(++cnt==N-1)
-					break;
+				Edge curr = pq.poll();
+				if(findParent(curr.from)!=findParent(curr.to)) {
+					sum += curr.cost;
+					union(curr.from, curr.to);
+				}
 			}
-			
-			System.out.printf("#%d %d\n",t, Math.round(ans*E));
+			System.out.printf("#%d ", tc);
+			System.out.println(Math.round(sum));
+			//System.out.printf("#%d %d\n",tc,sum);
 		}
-
-		br.close();
 	}
-	private static boolean union(int a, int b) {
-		int pa = find(a);
-		int pb = find(b);
+	static void union(int x, int y) {
+		int px = findParent(x);
+		int py = findParent(y);
 		
-		if(pa!=pb) {
-			parents[pb] = pa;
-			return false;
-		}
-		return true;
+		if(px<py)
+			parents[py] = px;
+		else parents[px] = py;
 	}
-	private static int find(int x) {
-		if(parents[x]==x) return x;
-		return parents[x] = find(parents[x]);
+	static int findParent(int x) {
+		while(x!=parents[x]) {
+			parents[x] = parents[parents[x]];
+			x = parents[x];
+		}
+		return x;
+	}
+	static void makeEdges() {
+		pq = new PriorityQueue<>();
+		
+		for(int i=0;i<N;i++) {
+			for(int j=0;j<N;j++)
+				pq.add(new Edge(i,j,cal(i,j)));
+		}
+	}
+	static double cal(int a, int b) {
+		//환경 부담 세율(E)과 각 해저터널 길이(L)의 제곱의 곱(E * L^2)만큼 지불
+		long len = (X[a]-X[b])*(X[a]-X[b])+(Y[a]-Y[b])*(Y[a]-Y[b]);
+		return (double)len * E;
+	}
+	private static void initialize(BufferedReader br) throws IOException {
+		N = Integer.parseInt(br.readLine().trim());
+		
+		X = new long[N];
+		Y = new long[N];
+		
+		StringTokenizer st;
+		st = new StringTokenizer(br.readLine());
+		for(int i=0;i<N;i++) {
+			X[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i=0;i<N;i++) {
+			Y[i] = Integer.parseInt(st.nextToken());
+		}
+		E = Double.parseDouble(br.readLine());
+		
+		parents = new int[N];
+		for(int i=0;i<N;i++)
+			parents[i] = i;
+	}
+	static class Edge implements Comparable<Edge>{
+		int from, to;
+		double cost;
+		
+		Edge(int f, int t, double c){
+			from = f;
+			to = t;
+			cost = c;
+		}
+		
+		@Override
+		public int compareTo(Edge o) {
+			return Double.compare(this.cost, o.cost);
+		}
 	}
 }
