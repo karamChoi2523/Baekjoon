6980029d3c54c25cab3b1efeae57c754bda5dbff
karam Choi <86296569+karamChoi2523@users.noreply.github.com>
2025-08-27 15:23:50 +0900

    [Gold III] Title: 게리맨더링, Time: 84 ms, Memory: 14700 KB -BaekjoonHub

--- 백준/Gold/17471. 게리맨더링/게리맨더링.java
+++ 백준/Gold/17471. 게리맨더링/게리맨더링.java
@@ -1,94 +1,103 @@
 import java.io.*;
 import java.util.*;
 
+
 public class Main {
 	static int N;
+	static int total;
 	static int[] person;
 	static ArrayList<Integer>[] adj;
+	static boolean[] v;
 	static int min;
-	static int total = 0;
-	static boolean[] visited;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		initialize(br);
+
+		min = total;
+		v = new boolean[N+1];
+		subset(1);
+
+		//pick을 하고 -> subset
+		//a구역과 b구역이 각각 연결이 되어있는지 확인하고
+		//연결되어있다면 그때 최솟값 체크
+
+		System.out.println(min>=total?-1:min);
+	}
+	static boolean check(boolean vVal) {
+		boolean[] visited = new boolean[N+1];
+		Queue<Integer> q = new ArrayDeque<>();
 		
-		N = Integer.parseInt(br.readLine());
-		person = new int[N+1];
-		adj = new ArrayList[N+1];
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		int start = -1;
 		for(int i=1;i<N+1;i++) {
-			adj[i] = new ArrayList<>();
-			person[i] = Integer.parseInt(st.nextToken());
-			total += person[i];
-		}
-		
-		for(int i=1;i<N+1;i++) {
-			st = new StringTokenizer(br.readLine());
-			int cnt = Integer.parseInt(st.nextToken());
-			
-			for(int j=0;j<cnt;j++) {
-				adj[i].add(Integer.parseInt(st.nextToken()));
+			if(v[i]==vVal) {
+				start = i;
+				break;
 			}
 		}
 		
-		visited = new boolean[N+1];
-		min = total;
-		pick(1);
+		if(start==-1) return false;
 		
-		System.out.println(min==total?-1:min);
-	}
-	public static void pick(int idx) {
-		if(idx==N+1) {
-            if(checkGroup('A') && checkGroup('B'))
-			    min = Math.min(min, calDiff());
-			return;
-		}
-		
-		visited[idx] = true;
-		pick(idx+1);
-		
-		visited[idx] = false;
-		pick(idx+1);
-	}
-	static boolean checkGroup(char c) {
-		boolean target = false;
-		if(c=='A')
-			target = true;
-		
-		ArrayList<Integer> list = new ArrayList<>();
-		for(int i=1;i<N+1;i++)
-			if(visited[i]==target)
-				list.add(i);
-        
-        if(list.size()==0||list.size()==N)
-            return false;
-		
-		Queue<Integer> q = new LinkedList<>();
-		boolean[] check = new boolean[N+1];
-		q.add(list.get(0));
-		check[list.get(0)] = true;
-		
-		int cnt=1;
+		visited[start] = true;
+		q.add(start);
+
 		while(!q.isEmpty()) {
 			int curr = q.poll();
-			
-			for(int next : adj[curr]) {
-				if(!check[next] && list.contains(next)) {
-					check[next] = true;
-					q.add(next);
-					cnt++;
+
+			for(int e : adj[curr]) {
+				if(v[e]==vVal && !visited[e]) {
+					visited[e] = true;
+					q.add(e);
 				}
 			}
 		}
-		
-		return cnt==list.size();
+
+		for(int i=1;i<N+1;i++) {
+			if(v[i] == vVal && !visited[i]) return false;
+		}
+		return true;
 	}
-	static int calDiff() {
-		int sum = 0;
+	static void subset(int idx) {
+		if(idx==N+1) {
+			if(check(true) && check(false)) {
+				int sumA = 0;
+				for(int i=1;i<N+1;i++)
+					if(v[i]) sumA+=person[i];
+				min = Math.min(min, Math.abs(sumA-(total-sumA)));
+			}
+			return;
+		}
+
+		v[idx] = true;
+		subset(idx+1);
+		v[idx] = false;
+		subset(idx+1);
+	}
+	private static void initialize(BufferedReader br) throws IOException {
+		N = Integer.parseInt(br.readLine());
+		person = new int[N+1];
+
+		StringTokenizer st;
+		st = new StringTokenizer(br.readLine());
+		total = 0;
+		for(int i=1;i<N+1;i++) {
+			person[i] = Integer.parseInt(st.nextToken());
+			total += person[i];
+		}
+
+		adj = new ArrayList[N+1];
 		for(int i=1;i<N+1;i++)
-			if(visited[i])
-				sum+=person[i];
-		
-		return Math.abs(total-sum*2);
+			adj[i] = new ArrayList<>();
+		for(int i=1;i<N+1;i++) {
+			st = new StringTokenizer(br.readLine());
+
+			int cnt = Integer.parseInt(st.nextToken());
+
+			for(int j=0;j<cnt;j++) {
+				int node = Integer.parseInt(st.nextToken());
+				adj[i].add(node);
+			}
+		}
+
 	}
 }
