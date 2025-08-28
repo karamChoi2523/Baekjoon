25d6007c5b9b85d13d066dbd3b0662cc19b21765
karam Choi <86296569+karamChoi2523@users.noreply.github.com>
2025-08-28 23:25:03 +0900

    [Gold III] Title: 봄버맨 2, Time: 400 ms, Memory: 21184 KB -BaekjoonHub

--- 백준/Gold/16919. 봄버맨 2/봄버맨 2.java
+++ 백준/Gold/16919. 봄버맨 2/봄버맨 2.java
@@ -1,91 +1,87 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-	static class Bomb implements Comparable<Bomb>{
-		int x, y, time;
-		
-		public Bomb(int x, int y) {
-			this.x = x;
-			this.y = y;
-			this.time = 3;
-		}
-		
-		@Override
-		public int compareTo(Bomb o) {
-			return this.time-o.time;
-		}
-	}
-	static int R,C,N;
-	static int[] dx = {1,-1,0,0};
-	static int[] dy = {0,0,1,-1};
-	static char[][][] versionMap;
+	static int R, C, N;
+	static int[] dx = {-1,1,0,0};
+	static int[] dy = {0,0,-1,1};
+	static char[][] res;
+	static char[][] res3;
+	static char[][] res5;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringTokenizer st;
 
+		st = new StringTokenizer(br.readLine());
 		R = Integer.parseInt(st.nextToken());
 		C = Integer.parseInt(st.nextToken());
 		N = Integer.parseInt(st.nextToken());
-		
-		versionMap = new char[3][R][C];
-		
-		//1초
+
+		res = new char[R][C];
+		res3 = new char[R][C];
+		res5 = new char[R][C];
 		for(int i=0;i<R;i++)
-			versionMap[0][i] = br.readLine().toCharArray();
-		//3초 5초
-		for(int i=0;i<2;i++) {
-			check(i, i+1);
-			make(i+1);
+			res[i] = br.readLine().toCharArray();
+
+		if(N==1) {
+			print(res);
+			return;
 		}
-		
+
 		if(N%2==0) {
 			for(int i=0;i<R;i++) {
 				for(int j=0;j<C;j++)
 					System.out.print('O');
 				System.out.println();
 			}
+			return;
+		}
+
+		res3 = explode(res);
+		res5 = explode(res3);
+
+		if(N%4==3) {
+			print(res3);
 		}else {
-			if(N==1) {
-				for(int i=0;i<R;i++) {
-					for(int j=0;j<C;j++)
-						System.out.print(versionMap[0][i][j]);
-					System.out.println();
-				}
-			}else if(N%4==3) {
-				for(int i=0;i<R;i++) {
-					for(int j=0;j<C;j++)
-						System.out.print(versionMap[1][i][j]);
-					System.out.println();
-				}
-			}else {
-				for(int i=0;i<R;i++) {
-					for(int j=0;j<C;j++)
-						System.out.print(versionMap[2][i][j]);
-					System.out.println();
-				}
-			}
+			print(res5);
 		}
 	}
-	static void make(int curr) {
-		for(int i=0;i<R;i++)
+	static char[][] explode(char[][] board) {
+		boolean[][] visited = new boolean[R][C];
+
+		for(int i=0;i<R;i++) {
 			for(int j=0;j<C;j++) {
-				if(versionMap[curr][i][j]=='.') continue;
-				versionMap[curr][i][j] = 'O';
-			}
-	}
-	static void check(int pre, int curr) {
-		for(int i=0;i<R;i++)
-			for(int j=0;j<C;j++)
-				if(versionMap[pre][i][j]=='O') {
-					versionMap[curr][i][j] = '.';
+				if(board[i][j]=='O') {
+					visited[i][j] = true;
 					for(int d=0;d<4;d++) {
 						int nx = i+dx[d];
 						int ny = j+dy[d];
-						
-						if(nx>=0 && nx<R && ny>=0 && ny<C)
-							versionMap[curr][nx][ny]='.';
+
+						if(checkNext(nx, ny))
+							visited[nx][ny] = true;
 					}
 				}
+			}
+		}
+		
+		char[][] ans = new char[R][C];
+
+		for(int i=0;i<R;i++) {
+			for(int j=0;j<C;j++)
+				ans[i][j] = visited[i][j]?'.':'O';
+		}
+		
+		return ans;
+	}
+	static void print(char[][] result) {
+		for(int i=0;i<R;i++) {
+			for(int j=0;j<C;j++)
+				System.out.print(result[i][j]);
+			System.out.println();
+		}
+	}
+	static boolean checkNext(int x, int y) {
+		if(x<0 || x>=R || y<0 || y>=C) return false;
+		return true;
 	}
 }
