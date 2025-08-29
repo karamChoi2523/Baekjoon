5d7f8a43f61303cbc9613d561bd12c5f6c2936b1
karam Choi <86296569+karamChoi2523@users.noreply.github.com>
2025-08-29 10:26:15 +0900

    [Unrated] Title: [모의 SW 역량테스트] 벽돌 깨기, Time: 161 ms, Memory: 50,684 KB -BaekjoonHub

--- SWEA/Unrated/5656. ［모의 SW 역량테스트］ 벽돌 깨기/［모의 SW 역량테스트］ 벽돌 깨기.java
+++ SWEA/Unrated/5656. ［모의 SW 역량테스트］ 벽돌 깨기/［모의 SW 역량테스트］ 벽돌 깨기.java
@@ -3,7 +3,6 @@ import java.util.*;
 
 public class Solution {
 	static int N, W, H;
-	static int[][] board;
 	static int[] dx = {-1,1,0,0};
 	static int[] dy = {0,0,-1,1};
 	static int min = Integer.MAX_VALUE;
@@ -15,14 +14,14 @@ public class Solution {
 		//int T = 10;
 		int T = Integer.parseInt(br.readLine().trim());
 		for(int tc=1;tc<=T;tc++) {			
-			initialize(br);
+			int[][] board = initialize(br);
 			
 			select = new int[N];
-			pick(0);
+			pick(0, board);
 			System.out.printf("#%d %d\n",tc,min);
 		}
 	}
-	static int[][] deepCopy(){
+	static int[][] deepCopy(int[][] board){
 		int[][] copy = new int[H][W];
 		
 		for(int i=0;i<H;i++)
@@ -30,25 +29,36 @@ public class Solution {
 	
 		return copy;
 	}
+	static boolean checkCol(int idx, int[][] board) {
+		for(int i=0;i<H;i++)
+			if(board[i][idx]!=0) return true;
+		return false;
+	}
 	//중복 순열
 	static int[] select;
-	static void pick(int idx) {
+	static void pick(int idx, int[][] board) {
+		int remain = countBrick(board);
+		if(remain==0) {
+			min = 0;
+			return;
+		}
 		if(idx==N) {
-			int[][] temp = deepCopy();
-			for(int i=0;i<select.length;i++)
-				temp = bfs(select[i], temp);
-			min = Math.min(min, countBrick(temp));
-			//System.out.println(Arrays.toString(select));
+			min = Math.min(min, remain);
 			return;
 		}
 		
 		for(int i=0;i<W;i++) {
-			select[idx] = i;
-			pick(idx+1);
+			if(checkCol(i, board)) {
+				//깨질 벽돌을 찾았으니 연쇄 작용 처리
+				int[][] newBoard = deepCopy(board);
+				bfs(i, newBoard);
+				pick(idx+1, newBoard);
+				if(min==0) return;
+			}
 		}
 	}
-	static int[][] bfs(int start, int[][] copy) {
-		Queue<int[]> q = new LinkedList<>();
+	static void bfs(int start, int[][] copy) {
+		Queue<int[]> q = new ArrayDeque<>();
 		int startR = -1;
 		for(int i=0;i<H;i++)
 			if(copy[i][start]!=0) {
@@ -56,14 +66,13 @@ public class Solution {
 				break;
 			}
 		
-		if(startR==-1) return copy;
+		if(startR==-1) return;
 		
 		q.add(new int[] {startR, start, copy[startR][start]});
 		while(!q.isEmpty()) {
 			int[] curr = q.poll();
 			//공격
-			for(int d=0;d<4;d++) {
-				
+			for(int d=0;d<4;d++) {				
 				for(int i=1;i<curr[2];i++) {
 					int nx = curr[0]+dx[d]*i;
 					int ny = curr[1]+dy[d]*i;
@@ -79,19 +88,14 @@ public class Solution {
 		}
 		//벽돌 내려옴,,
 		for(int i=0;i<W;i++) {
-			ArrayList<Integer> list = new ArrayList<>();
+			int idx = H-1;
 			for(int j=H-1;j>=0;j--)
 				if(copy[j][i]!=0) {
-					list.add(copy[j][i]);
+					int val = copy[j][i];
 					copy[j][i] = 0;
+					copy[idx--][i] = val;
 				}
-			int idx = 0;
-			for(int j=H-1;j>=0;j--) {
-				if(idx>=list.size()) break;
-				copy[j][i] = list.get(idx++);
-			}
 		}
-		return copy;
 	}
 	static boolean checkNext(int x, int y) {
 		if(x<0 || x>=H || y<0 || y>=W) return false;
@@ -105,7 +109,7 @@ public class Solution {
 		
 		return cnt;
 	}
-	private static void initialize(BufferedReader br) throws IOException {
+	private static int[][] initialize(BufferedReader br) throws IOException {
 		StringTokenizer st;
 		st = new StringTokenizer(br.readLine());
 
@@ -113,7 +117,7 @@ public class Solution {
 		W = Integer.parseInt(st.nextToken());
 		H = Integer.parseInt(st.nextToken());
 		
-		board = new int[H][W];
+		int[][] board = new int[H][W];
 		min = Integer.MAX_VALUE;
 
 		for(int i=0;i<H;i++) {
@@ -121,5 +125,7 @@ public class Solution {
 			for(int j=0;j<W;j++)
 				board[i][j] = Integer.parseInt(st.nextToken());
 		}
+		
+		return board;
 	}
 }
