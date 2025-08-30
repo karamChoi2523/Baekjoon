a05eb6cb80eb61bbf714ede1173ebe51deb0c01d
karam Choi <86296569+karamChoi2523@users.noreply.github.com>
2025-08-30 16:51:39 +0900

    [Unrated] Title: [SW Test 샘플문제] 프로세서 연결하기, Time: 3,545 ms, Memory: 31,796 KB -BaekjoonHub

--- SWEA/Unrated/1767. ［SW Test 샘플문제］ 프로세서 연결하기/［SW Test 샘플문제］ 프로세서 연결하기.java
+++ SWEA/Unrated/1767. ［SW Test 샘플문제］ 프로세서 연결하기/［SW Test 샘플문제］ 프로세서 연결하기.java
@@ -1,116 +1,96 @@
 import java.io.*;
 import java.util.*;
 
-
 public class Solution {
+	static class Cell{
+		int x, y;
+
+		Cell(int x, int y){
+			this.x = x;
+			this.y = y;
+		}
+	}
 	static int N;
-	static int[][] board;
+	static int[] dx = {-1,1,0,0};
+	static int[] dy = {0,0,-1,1};
+	static ArrayList<Cell> cells;
+	static int maxCnt;
+	static int minTotal;
 	static boolean[][] visited;
-	static int min = Integer.MAX_VALUE;
-	static int maxCnt = 0;
-	static ArrayList<int[]> cores;
+	static int[][] board;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		int T = Integer.parseInt(br.readLine().trim());
 		//int T = 10;
-		for(int tc=1;tc<=T;tc++) {
+		int T = Integer.parseInt(br.readLine().trim());
+		for(int tc=1;tc<=T;tc++) {			
 			initialize(br);
 
-			visited = new boolean[N][N];
-			min = Integer.MAX_VALUE;
-			maxCnt = 0;
-			sol(0, 0, 0);
+			dfs(0, 0, 0);
 
-			System.out.printf("#%d %d\n",tc,min);
+			System.out.printf("#%d %d\n",tc,minTotal);
 		}
 	}
-	static void sol(int idx, int curr, int cnt) {
-		if(idx==cores.size()) {
+	static void dfs(int idx, int cnt, int total) {
+		if(idx==cells.size()) {
 			if(cnt > maxCnt) {
 				maxCnt = cnt;
-				min = curr;
-			}else if(cnt == maxCnt) {
-				min = Math.min(min, curr);
+				minTotal = total;
+			}else if(cnt==maxCnt) {
+				minTotal = Math.min(minTotal, total);
 			}
 			return;
 		}
 
-		int[] pos = cores.get(idx);
-
-		int x = pos[0];
-		int y = pos[1];
+		Cell curr = cells.get(idx);
 
 		for(int d=0;d<4;d++) {
-			int len = check(x,y, d);
+			int len = check(curr.x, curr.y, d);
 			if(len==-1) {
-				sol(idx+1, curr, cnt);
+				dfs(idx+1, cnt, total);
 			}else {
-				toggle(x, y, d);
-				sol(idx+1, curr+len, cnt+1);
-				toggle(x, y, d);
-			}
-		}
-
-	}
-	static void toggle(int x, int y, int dir) {
-		if(dir==0) {
-			while(x-->0) {
-				visited[x][y] = visited[x][y]?false:true;
-			}
-		}else if(dir==1) {
-			while(x++<N-1) {
-				visited[x][y] = visited[x][y]?false:true;
-			}
-		}else if(dir==2) {
-			while(y-->0) {
-				visited[x][y] = visited[x][y]?false:true;
-			}
-		}else if(dir==3) {
-			while(y++<N-1) {
-				visited[x][y] = visited[x][y]?false:true;
+				toggle(curr.x, curr.y, d);
+				dfs(idx+1, cnt+1, total+len);
+				toggle(curr.x, curr.y, d);
 			}
 		}
 	}
-	static int check(int x, int y, int dir) {
+	static void toggle(int x, int y, int d) {
+		while(true) {			
+			x+=dx[d];
+			y+=dy[d];
+			if(x<0 || y<0 || x>=N || y>=N) break;
+			visited[x][y] = !visited[x][y];
+		}
+	}
+	static int check(int x, int y, int d) {
 		int cnt = 0;
-		if(dir==0) {
-			while(x-->0) {
-				if(board[x][y]==1 || visited[x][y]) return -1;
-				cnt++;
-			}
-		}else if(dir==1) {
-			while(x++<N-1) {
-				if(board[x][y]==1 || visited[x][y]) return -1;
-				cnt++;
-			}
-		}else if(dir==2) {
-			while(y-->0) {
-				if(board[x][y]==1 || visited[x][y]) return -1;
-				cnt++;
-			}
-		}else if(dir==3) {
-			while(y++<N-1) {
-				if(board[x][y]==1 || visited[x][y]) return -1;
-				cnt++;
-			}
+		
+		while(true) {			
+			x+=dx[d];
+			y+=dy[d];
+			if(x<0 || y<0 || x>=N || y>=N) break;
+			if(visited[x][y] || board[x][y]==1) return -1;
+			cnt++;
 		}
 		return cnt;
 	}
-	private static void initialize(BufferedReader br) throws IOException {
+	private static void initialize(BufferedReader br) throws IOException {		
 		N = Integer.parseInt(br.readLine().trim());
-		cores = new ArrayList<>();
 		StringTokenizer st;
+
+		maxCnt = 0;
+		minTotal = Integer.MAX_VALUE;
+		visited = new boolean[N][N];
+		cells = new ArrayList<>();
 		board = new int[N][N];
+
 		for(int i=0;i<N;i++) {
 			st = new StringTokenizer(br.readLine());
 			for(int j=0;j<N;j++) {
 				board[i][j] = Integer.parseInt(st.nextToken());
-				if(board[i][j]==1)
-					if(board[i][j]==1) {
-						if(i==0 || j==0 || i==N-1 || j==N-1) continue;
-						cores.add(new int[] {i,j});
-					}
+				if(board[i][j]==1 && i!=0 && j!=0)
+					cells.add(new Cell(i,j));
 			}
 		}
 	}
