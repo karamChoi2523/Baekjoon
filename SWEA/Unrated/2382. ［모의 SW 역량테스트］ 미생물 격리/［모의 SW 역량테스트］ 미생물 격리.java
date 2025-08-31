08ce6ec6a67f45eda792ba2e057b1a6115851e2f
karam Choi <86296569+karamChoi2523@users.noreply.github.com>
2025-08-31 17:26:58 +0900

    [Unrated] Title: [모의 SW 역량테스트] 미생물 격리, Time: 485 ms, Memory: 102,596 KB -BaekjoonHub

--- SWEA/Unrated/2382. ［모의 SW 역량테스트］ 미생물 격리/［모의 SW 역량테스트］ 미생물 격리.java
+++ SWEA/Unrated/2382. ［모의 SW 역량테스트］ 미생물 격리/［모의 SW 역량테스트］ 미생물 격리.java
@@ -3,100 +3,103 @@ import java.util.*;
 
 public class Solution {
 	static int N, M, K;
-	static Queue<Cell> q;
 	static int[] dx = {0,-1,1,0,0};
 	static int[] dy = {0,0,0,-1,1};
-	//1 2 3 4
+	static Queue<Cell> cells;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
 
-		StringBuilder sb = new StringBuilder();
 		//int T = 10;
 		int T = Integer.parseInt(br.readLine().trim());
 		for(int tc=1;tc<=T;tc++) {			
 			initialize(br);
-
-			bfs();
 			
-			int total = 0;
-			while(!q.isEmpty()) {
-				total += q.poll().cnt;
+			while(M-->0)
+				sol();
+			
+			int res = 0;
+			while(!cells.isEmpty()) {
+				res += cells.poll().cnt;
 			}
 
-			System.out.printf("#%d %d\n",tc,total);
+			System.out.printf("#%d %d\n",tc,res);
 		}
 	}
-	static void bfs() {
-		while(M-->0) {
-			int size = q.size();
-			HashMap<Pos, ArrayList<Cell>> hMap = new HashMap<>();
-			for(int step=0;step<size;step++) {
-				Cell cell = q.poll();
-				
-				int nx = cell.x+dx[cell.d];
-				int ny = cell.y+dy[cell.d];
-				
-				if(!checkBoard(nx, ny)) {
-					cell.cnt/=2;
-					if(cell.cnt==0) continue;
-					if(cell.d==1) cell.d = 2;
-					else if(cell.d==2) cell.d = 1;
-					else if(cell.d==3) cell.d = 4;
-					else cell.d = 3;
-				}
-				
-				if(hMap.containsKey(new Pos(nx, ny)))
-					hMap.get(new Pos(nx, ny)).add(new Cell(nx, ny, cell.cnt, cell.d));
-				else {
-					ArrayList<Cell> temp = new ArrayList<>();
-					temp.add(new Cell(nx, ny, cell.cnt, cell.d));
-					hMap.put(new Pos(nx, ny), temp);
-				}
-			}
+	static void sol() {
+		HashMap<Pos, ArrayList<Cell>> hMap = new HashMap<>();
+		int size = cells.size();
+		
+		for(int step=0;step<size;step++) {
+			Cell cell = cells.poll();
+			int nx = cell.x+dx[cell.dir];
+			int ny = cell.y+dy[cell.dir];
 			
-			for(Pos p : hMap.keySet()) {
-				ArrayList<Cell> curr = hMap.get(p);
-				if(curr.size()==1) {
-					q.add(curr.get(0));
-				}else {
-					int max = -1;
-					int dIdx = -1;
-					int cnt = 0;
-					for(Cell cell : curr) {
-						cnt += cell.cnt;
-						if(max < cell.cnt) {
-							max = cell.cnt;
-							dIdx = cell.d;
-						}
+			if(!checkNext(nx, ny)) {
+				if(cell.dir==1) cell.dir = 2;
+				else if(cell.dir==2) cell.dir = 1;
+				else if(cell.dir==3) cell.dir = 4;
+				else cell.dir = 3;
+				cell.cnt /=2;
+				if(cell.cnt==0) continue;
+				
+				cell.x = nx;
+				cell.y = ny;
+				cells.add(cell);
+			}else {
+				cell.x = nx;
+				cell.y = ny;
+				
+				Pos key = new Pos(nx, ny);
+				if(!hMap.containsKey(key))
+					hMap.put(key, new ArrayList<Cell>());
+				hMap.get(key).add(cell);
+			}
+		}
+		
+		for(Pos key : hMap.keySet()) {
+			ArrayList<Cell> list = hMap.get(key);
+			
+			if(list.size()==1) {
+				cells.add(list.get(0));
+			}else {
+				int sum = 0;
+				int dir = 0;
+				int max = -1;
+				for(Cell c : list) {
+					sum += c.cnt;
+					if(c.cnt>max) {
+						max = c.cnt;
+						dir = c.dir;
 					}
-					q.add(new Cell(p.x, p.y, cnt, dIdx));
 				}
+				cells.add(new Cell(key.x, key.y, sum, dir));
 			}
 		}
 	}
-	static boolean checkBoard(int x, int y) {
-		if(x>0 && x<N-1 && y>0 && y<N-1) return true;
-		return false;
+	static boolean checkNext(int x, int y) {
+		if(x<1 || x>=N-1 || y<1 || y>=N-1) return false;
+		return true;
 	}
-	private static void initialize(BufferedReader br) throws IOException {
+	private static void initialize(BufferedReader br) throws IOException {		
 		StringTokenizer st;
 		st = new StringTokenizer(br.readLine());
 
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(st.nextToken());
-
-		q = new LinkedList<>();
+		cells = new ArrayDeque<>();
+		
 		for(int i=0;i<K;i++) {
 			st = new StringTokenizer(br.readLine());
+
 			int x = Integer.parseInt(st.nextToken());
 			int y = Integer.parseInt(st.nextToken());
 			int cnt = Integer.parseInt(st.nextToken());
-			int d = Integer.parseInt(st.nextToken());
-
-			q.add(new Cell(x,y,cnt,d));
+			int dir = Integer.parseInt(st.nextToken());
+			
+			cells.add(new Cell(x,y,cnt,dir));
 		}
+		
 	}
 	static class Pos{
 		int x, y;
@@ -108,23 +111,23 @@ public class Solution {
 		
 		@Override
 		public boolean equals(Object obj) {
-			Pos p = (Pos) obj;
-			return this.x==p.x && this.y == p.y;
+			Pos p = (Pos)obj;
+			return this.x==p.x && this.y==p.y;
 		}
 		
 		@Override
 		public int hashCode() {
-			return Objects.hash(x, y);
+			return Objects.hash(x,y);
 		}
 	}
 	static class Cell{
-		int x, y, cnt, d;
-
-		Cell(int x, int y, int cnt, int d){
+		int x, y, cnt, dir;
+		
+		Cell(int x, int y, int cnt, int dir){
 			this.x = x;
 			this.y = y;
 			this.cnt = cnt;
-			this.d = d;
+			this.dir = dir;
 		}
 	}
 }
