dd99b507c88030cfc0427f8d41fd714ace1052d0
karam Choi <86296569+karamChoi2523@users.noreply.github.com>
2025-09-02 08:49:20 +0900

    [Gold III] Title: 게리맨더링, Time: 80 ms, Memory: 13808 KB -BaekjoonHub

--- 백준/Gold/17471. 게리맨더링/게리맨더링.java
+++ 백준/Gold/17471. 게리맨더링/게리맨더링.java
@@ -1,36 +1,69 @@
 import java.io.*;
 import java.util.*;
 
-
 public class Main {
 	static int N;
-	static int total;
 	static int[] person;
 	static ArrayList<Integer>[] adj;
-	static boolean[] v;
+	static boolean[] visited;
 	static int min;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		initialize(br);
-
-		min = total;
-		v = new boolean[N+1];
+		
+		N = Integer.parseInt(br.readLine());
+		
+		person = new int[N+1];
+		StringTokenizer st;
+		st = new StringTokenizer(br.readLine());
+		for(int i=1;i<N+1;i++) {
+			person[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		
+		adj = new ArrayList[N+1];
+		for(int i=1;i<N+1;i++)
+			adj[i] = new ArrayList<>();
+		
+		for(int i=1;i<N+1;i++) {
+			st = new StringTokenizer(br.readLine());
+			int cnt = Integer.parseInt(st.nextToken());
+			for(int j=0;j<cnt;j++)
+				adj[i].add(Integer.parseInt(st.nextToken()));
+		}
+		
+		min = Integer.MAX_VALUE;
+		visited = new boolean[N+1];
 		subset(1);
-
-		//pick을 하고 -> subset
-		//a구역과 b구역이 각각 연결이 되어있는지 확인하고
-		//연결되어있다면 그때 최솟값 체크
-
-		System.out.println(min>=total?-1:min);
+		System.out.println(min==Integer.MAX_VALUE?-1:min);
 	}
-	static boolean check(boolean vVal) {
-		boolean[] visited = new boolean[N+1];
+	static void subset(int idx) {
+		if(idx==N+1) {
+			if(checkConnection(true) && checkConnection(false)) {
+				int sumA = 0;
+				int sumB = 0;
+				
+				for(int i=1;i<N+1;i++)
+					if(visited[i]) sumA+=person[i];
+					else sumB+=person[i];
+				
+				min = Math.min(min, Math.abs(sumA-sumB));
+			}
+			return;
+		}
+		
+		visited[idx] = true;
+		subset(idx+1);
+		visited[idx] = false;
+		subset(idx+1);
+		
+	}
+	static boolean checkConnection(boolean target) {
 		Queue<Integer> q = new ArrayDeque<>();
+		boolean[] v = new boolean[N+1];
 		
 		int start = -1;
 		for(int i=1;i<N+1;i++) {
-			if(v[i]==vVal) {
+			if(visited[i]==target) {
 				start = i;
 				break;
 			}
@@ -38,66 +71,25 @@ public class Main {
 		
 		if(start==-1) return false;
 		
-		visited[start] = true;
 		q.add(start);
-
+		v[start] = true;
+		
 		while(!q.isEmpty()) {
 			int curr = q.poll();
-
+			
 			for(int e : adj[curr]) {
-				if(v[e]==vVal && !visited[e]) {
-					visited[e] = true;
+				if(visited[e]==target && !v[e]) {
+					v[e] = true;
 					q.add(e);
 				}
 			}
 		}
-
+		
+		
 		for(int i=1;i<N+1;i++) {
-			if(v[i] == vVal && !visited[i]) return false;
+			if(visited[i]==target && !v[i])
+				return false;
 		}
 		return true;
-	}
-	static void subset(int idx) {
-		if(idx==N+1) {
-			if(check(true) && check(false)) {
-				int sumA = 0;
-				for(int i=1;i<N+1;i++)
-					if(v[i]) sumA+=person[i];
-				min = Math.min(min, Math.abs(sumA-(total-sumA)));
-			}
-			return;
-		}
-
-		v[idx] = true;
-		subset(idx+1);
-		v[idx] = false;
-		subset(idx+1);
-	}
-	private static void initialize(BufferedReader br) throws IOException {
-		N = Integer.parseInt(br.readLine());
-		person = new int[N+1];
-
-		StringTokenizer st;
-		st = new StringTokenizer(br.readLine());
-		total = 0;
-		for(int i=1;i<N+1;i++) {
-			person[i] = Integer.parseInt(st.nextToken());
-			total += person[i];
-		}
-
-		adj = new ArrayList[N+1];
-		for(int i=1;i<N+1;i++)
-			adj[i] = new ArrayList<>();
-		for(int i=1;i<N+1;i++) {
-			st = new StringTokenizer(br.readLine());
-
-			int cnt = Integer.parseInt(st.nextToken());
-
-			for(int j=0;j<cnt;j++) {
-				int node = Integer.parseInt(st.nextToken());
-				adj[i].add(node);
-			}
-		}
-
 	}
 }
