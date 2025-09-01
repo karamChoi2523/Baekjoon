2f702b4d84443f8076255357e47909d7f1bf953b
karam Choi <86296569+karamChoi2523@users.noreply.github.com>
2025-09-01 17:38:11 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 보급로, Time: 126 ms, Memory: 31,372 KB -BaekjoonHub

--- SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
+++ SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
@@ -1,56 +1,75 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Solution {
 	static int N;
 	static int[][] board;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
 		int T = Integer.parseInt(br.readLine());
-
 		for(int tc=1;tc<=T;tc++) {
 			N = Integer.parseInt(br.readLine());
+			
 			board = new int[N][N];
 			for(int i=0;i<N;i++) {
 				char[] temp = br.readLine().toCharArray();
-				for(int j=0;j<N;j++) {
+				for(int j=0;j<N;j++)
 					board[i][j] = temp[j]-'0';
-				}
 			}
 			
-			System.out.printf("#%d %d\n",tc, dijkstra());
+			
+			int res = dijkstra();
+			
+			System.out.printf("#%d %d\n",tc, res);
 		}
+		
+		
 	}
 	static int[] dx = {-1,1,0,0};
 	static int[] dy = {0,0,-1,1};
-	static long dijkstra() {
-		Queue<int[]> q = new LinkedList<>();
-		long[][] dist = new long[N][N];
+	static int dijkstra() {
+		Queue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>() {
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				return o1[2]-o2[2];
+			}
+		});
 		
+		int[][] dist = new int[N][N];
 		for(int i=0;i<N;i++)
-			Arrays.fill(dist[i], Long.MAX_VALUE);
-		q.add(new int[] {0,0});
+			Arrays.fill(dist[i], Integer.MAX_VALUE);
+		boolean[][] visited = new boolean[N][N];
+		pq.add(new int[] {0,0,0});
 		dist[0][0] = 0;
 		
-		while(!q.isEmpty()) {
-			int[] curr = q.poll();
-			int cx = curr[0];
-			int cy = curr[1];
+		while(!pq.isEmpty()) {
+			int[] curr = pq.poll();
+			int x = curr[0];
+			int y = curr[1];
+			int time = curr[2];
 			
-			for(int i=0;i<4;i++) {
-				int nx = cx + dx[i];
-				int ny = cy + dy[i];
+			if(visited[x][y]) continue;
+			visited[x][y] = true;
+			
+			if(x==N-1 && y==N-1)
+				return time;
+			
+			for(int d=0;d<4;d++) {
+				int nx = x+dx[d];
+				int ny = y+dy[d];
 				
-				if(nx>=0 && nx<N && ny>=0 && ny<N) {
-					long cost = dist[cx][cy]+board[nx][ny];
-					
-					if(cost >= dist[nx][ny]) continue;
-					dist[nx][ny] = cost;
-					q.add(new int[] {nx, ny});
-				}
+				if(!checkNext(nx, ny)||visited[nx][ny]) continue;
+				int cost = dist[x][y]+board[nx][ny];
+				if(dist[nx][ny] < cost) continue;
+				dist[nx][ny] = cost;
+				pq.add(new int[] {nx, ny, cost});
 			}
 		}
-		
 		return dist[N-1][N-1];
+	}
+	static boolean checkNext(int x, int y) {
+		if(x<0 || x>=N || y<0 || y>=N) return false;
+		return true;
 	}
 }
