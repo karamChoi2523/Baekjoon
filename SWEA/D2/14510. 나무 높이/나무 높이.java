3d7eff9520e667a88341de79da563c5210c34c35
karam Choi <86296569+karamChoi2523@users.noreply.github.com>
2025-09-01 23:33:50 +0900

    [D2] Title: 나무 높이, Time: 89 ms, Memory: 26,880 KB -BaekjoonHub

--- SWEA/D2/14510. 나무 높이/나무 높이.java
+++ SWEA/D2/14510. 나무 높이/나무 높이.java
@@ -1,28 +1,27 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Solution {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		int T = Integer.parseInt(br.readLine());
+		
 		for(int tc=1;tc<=T;tc++) {
-			int N = Integer.parseInt(br.readLine());
-			int[] arr = new int[N];
 			
-			StringTokenizer st;
-			st = new StringTokenizer(br.readLine());
-			int maxH = -1;
+			int N = Integer.parseInt(br.readLine());
+			int[] tree = new int[N];
+			int max = -1;
+			StringTokenizer st = new StringTokenizer(br.readLine());
 			for(int i=0;i<N;i++) {
-				arr[i] = Integer.parseInt(st.nextToken());
-				if(maxH < arr[i])
-					maxH = arr[i];
+				tree[i] = Integer.parseInt(st.nextToken());
+				max = Math.max(max, tree[i]);
 			}
 			
 			int even = 0;
 			int odd = 0;
 			for(int i=0;i<N;i++) {
-				int diff = maxH - arr[i];
+				int diff = max-tree[i];
 				
 				if(diff==0) continue;
 				
@@ -31,20 +30,18 @@ public class Solution {
 			}
 			
 			int min = Math.min(even, odd);
-			even -= min;
-			odd -= min;
 			
 			int day = min*2;
 			
-			if(odd>0) {
-				day += 2*odd-1;
-			}
+			even -= min;
+			odd -= min;
 			
-			if(even>0){
-				day += even+1+(even-1)/3;
-			}
+			if(odd>0)
+				day += odd*2-1;
+			if(even>0)
+				day += even+1+(even-1)/3;			
 			
-			System.out.printf("#%d %d\n",tc,day);
+			System.out.printf("#%d %d\n", tc, day);
 		}
 	}
 }
