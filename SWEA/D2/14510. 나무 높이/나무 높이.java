ff09615b9a5b74cfa8d319c81ae5cecde1158781
karam Choi <86296569+karamChoi2523@users.noreply.github.com>
2025-09-02 12:07:48 +0900

    [D2] Title: 나무 높이, Time: 94 ms, Memory: 26,752 KB -BaekjoonHub

--- SWEA/D2/14510. 나무 높이/나무 높이.java
+++ SWEA/D2/14510. 나무 높이/나무 높이.java
@@ -4,15 +4,16 @@ import java.io.*;
 public class Solution {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+		StringTokenizer st;
 		int T = Integer.parseInt(br.readLine());
 		
 		for(int tc=1;tc<=T;tc++) {
-			
 			int N = Integer.parseInt(br.readLine());
+			
 			int[] tree = new int[N];
+			
 			int max = -1;
-			StringTokenizer st = new StringTokenizer(br.readLine());
+			st = new StringTokenizer(br.readLine());
 			for(int i=0;i<N;i++) {
 				tree[i] = Integer.parseInt(st.nextToken());
 				max = Math.max(max, tree[i]);
@@ -30,18 +31,17 @@ public class Solution {
 			}
 			
 			int min = Math.min(even, odd);
-			
 			int day = min*2;
 			
-			even -= min;
-			odd -= min;
+			even-=min;
+			odd-=min;
 			
 			if(odd>0)
-				day += odd*2-1;
+				day += 2*odd-1;
 			if(even>0)
-				day += even+1+(even-1)/3;			
+				day += even+1+(even-1)/3;
 			
-			System.out.printf("#%d %d\n", tc, day);
+			System.out.printf("#%d %d\n",tc,day);
 		}
 	}
 }
