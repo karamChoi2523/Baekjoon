686d21e7b4df9e38375b02abac0350cd444654f2
karam Choi <86296569+karamChoi2523@users.noreply.github.com>
2025-09-02 12:01:24 +0900

    [Unrated] Title: [모의 SW 역량테스트] 등산로 조성, Time: 101 ms, Memory: 26,880 KB -BaekjoonHub

--- SWEA/Unrated/1949. ［모의 SW 역량테스트］ 등산로 조성/［모의 SW 역량테스트］ 등산로 조성.java
+++ SWEA/Unrated/1949. ［모의 SW 역량테스트］ 등산로 조성/［모의 SW 역량테스트］ 등산로 조성.java
@@ -1,33 +1,49 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Solution {
 	static int N, K;
 	static int[][] board;
-	static ArrayList<int[]> startPosList;
+	static int res = -1;
 	static int[] dx = {-1,1,0,0};
 	static int[] dy = {0,0,-1,1};
-	static int res;
-	static boolean[][] visited;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		//int T = 10;
+		StringTokenizer st;
 		int T = Integer.parseInt(br.readLine().trim());
-		for(int tc=1;tc<=T;tc++) {			
-			initialize(br);
+		for(int tc=1;tc<=T;tc++) {
+			st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			K = Integer.parseInt(st.nextToken());
 			
-			res = -1;
-			for(int[] start : startPosList) {
-				visited = new boolean[N][N];
-				visited[start[0]][start[1]] = true;
-				dfs(start[0], start[1], 1, false);
+			board = new int[N][N];
+			
+			int max = -1;
+			ArrayList<int[]> posList = new ArrayList<>();
+			
+			for(int i=0;i<N;i++) {
+				st = new StringTokenizer(br.readLine());
+				for(int j=0;j<N;j++) {
+					board[i][j] = Integer.parseInt(st.nextToken());
+					if(board[i][j]==max) {
+						posList.add(new int[] {i,j});
+					}else if(board[i][j]>max) {
+						max = board[i][j];
+						posList = new ArrayList<>();
+						posList.add(new int[] {i,j});
+					}
+				}
 			}
-			
-			System.out.printf("#%d %d\n",tc,res);
+			res = -1;
+			for(int[] start : posList) {
+				boolean[][] visited = new boolean[N][N];
+				visited[start[0]][start[1]] = true;
+				backt(start[0], start[1], 1, visited, false);
+			}
+			System.out.printf("#%d %d\n", tc, res);
 		}
 	}
-	static void dfs(int x, int y, int len, boolean dig) {
+	private static void backt(int x, int y, int len, boolean[][] visited, boolean dig) {
 		res = Math.max(res, len);
 		
 		for(int d=0;d<4;d++) {
@@ -35,18 +51,20 @@ public class Solution {
 			int ny = y+dy[d];
 			
 			if(!checkNext(nx, ny) || visited[nx][ny]) continue;
+			
 			if(board[nx][ny]>=board[x][y]) {
-				int diff = board[nx][ny]-board[x][y];
+				int diff = board[nx][ny] - board[x][y];
+				
 				if(diff+1<=K && !dig) {
 					board[nx][ny] -= (diff+1);
 					visited[nx][ny] = true;
-					dfs(nx, ny, len+1, true);
+					backt(nx, ny, len+1, visited, true);
 					board[nx][ny] += (diff+1);
 					visited[nx][ny] = false;
 				}
-			}else {
+			}else{
 				visited[nx][ny] = true;
-				dfs(nx, ny, len+1, dig);
+				backt(nx, ny, len+1, visited, dig);
 				visited[nx][ny] = false;
 			}
 		}
@@ -54,28 +72,5 @@ public class Solution {
 	static boolean checkNext(int x, int y) {
 		if(x<0 || x>=N || y<0 || y>=N) return false;
 		return true;
-	}
-	private static void initialize(BufferedReader br) throws IOException {		
-		StringTokenizer st;
-		
-		st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		K = Integer.parseInt(st.nextToken());
-		startPosList = new ArrayList<>();
-		int max = -1;
-		
-		board = new int[N][N];
-		for(int i=0;i<N;i++) {
-			st = new StringTokenizer(br.readLine());
-			for(int j=0;j<N;j++) {
-				board[i][j] = Integer.parseInt(st.nextToken());
-				if(board[i][j]>max) {
-					startPosList = new ArrayList<>();
-					max = board[i][j];
-					startPosList.add(new int[] {i,j});
-				}else if(board[i][j]==max)
-					startPosList.add(new int[] {i,j});
-			}
-		}
 	}
 }
