ca390cbbd67440209b9ab55d4af494708a2ec5b7
karam Choi <86296569+karamChoi2523@users.noreply.github.com>
2025-09-03 14:57:28 +0900

    [Silver III] Title: 1로 만들기, Time: 148 ms, Memory: 41540 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,37 +1,36 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-	public static int min = Integer.MAX_VALUE;
-    static int[] dp;
-	
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		 BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		 Integer n = Integer.parseInt(bf.readLine());
-		 dp=new int[n+1];
-        
-       
-        dp[1]=0;
-        if(n>1)dp[2]=1;
-        if(n>2)dp[3]=1;
-        
-        for(int i=4;i<n+1;i++){
-            dp[i] = dp[i-1]+1;
-            if(i%3==0)
-                dp[i]=Math.min(dp[i],dp[i/3]+1);
-            if(i%2==0)
-                dp[i]=Math.min(dp[i],dp[i/2]+1);
-            }
-        
-       
-         
-        System.out.println(dp[n]);
-        
+	static int[] dp;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
+		int X = sc.nextInt();
+		dp = new int[X+1];
+		Arrays.fill(dp, (int)1e9);
+		dp[1] = 0;
+		if(X>1) dp[2] = 1;
+		if(X>2) dp[3] = 1;
+		if(X>3) dp[4] = 2;
+		
+		int res = sol(X);
+		
+		System.out.println(res);
 	}
-	
-	
+	static int sol(int x) {
+		if(x==1)
+			return 0;
+		
+		if(dp[x]!=(int)1e9)
+			return dp[x];
+
+		if(x%3==0)
+			dp[x] = Math.min(dp[x], sol(x/3)+1);
+		if(x%2==0)
+			dp[x] = Math.min(dp[x], sol(x/2)+1);
+		dp[x] = Math.min(dp[x], sol(x-1)+1);
+		
+		return dp[x];
+	}
 }
