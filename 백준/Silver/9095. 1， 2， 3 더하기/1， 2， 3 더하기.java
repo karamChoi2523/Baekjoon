f205909a3b7df1e6dbf069bb57cde2bb67ae7e0f
karam Choi <86296569+karamChoi2523@users.noreply.github.com>
2025-09-03 17:07:26 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 92 ms, Memory: 12860 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -1,32 +1,27 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int[] dp;
-    
-	public static void main(String[] args) throws NumberFormatException, IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static int[] dp;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 
-		int t = Integer.valueOf(br.readLine());
-		dp = new int[12];
-		
-		solution(11);
-		
-		while(t-->0) {
-			int n = Integer.valueOf(br.readLine());
-			
+		int T = sc.nextInt();
+
+		while(T-->0) {
+
+			int n = sc.nextInt();
+
+			dp = new int[11];
+			dp[1] = 1;
+			dp[2] = 2;
+			dp[3] = 4;
+			dp[4] = 7;
+
+			for(int i=4;i<11;i++)
+				dp[i] = dp[i-1]+dp[i-2]+dp[i-3];
+
 			System.out.println(dp[n]);
 		}
-	}
-
-	static void solution(int n) {
-		dp[1] = 1;
-		if(n>1) dp[2] = 2;
-		if(n>2) dp[3] = 4;
-		if(n>3) dp[4] = 7;
-		
-		for(int i=4;i<n+1;i++)
-			dp[i] = dp[i-1]+dp[i-2]+dp[i-3];
-		
-	}
-
+	}  
 }
