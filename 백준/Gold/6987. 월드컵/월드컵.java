e393ee0364c439a5bef6c2af03dde3260c1f9c46
karam Choi <86296569+karamChoi2523@users.noreply.github.com>
2025-09-04 14:44:33 +0900

    [Gold IV] Title: 월드컵, Time: 64 ms, Memory: 11576 KB -BaekjoonHub

--- 백준/Gold/6987. 월드컵/월드컵.java
+++ 백준/Gold/6987. 월드컵/월드컵.java
@@ -1,98 +1,100 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-	static class Country{
-		int win, mm, lose;
-		
-		public Country(int win, int mm, int lose) {
-			this.win = win;
-			this.mm = mm;
-			this.lose = lose;
-		}
-	}
-	static ArrayList<Country> countries;
-	static int maxCnt = 5*6/2;
-	static int[][] match = new int[maxCnt][2];
+	static int[] winArr;
+	static int[] mmArr;
+	static int[] loseArr;
+	static int[][] pairs = {
+			{0,1},{0,2},{0,3},{0,4},{0,5},{1,2},{1,3},{1,4},{1,5},{2,3},{2,4},{2,5},{3,4},{3,5},{4,5}	
+	};
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-				
-		int index = 0;
-		for(int i=0;i<5;i++)
-			for(int j=i+1;j<6;j++) {
-				match[index][0] = i;
-				match[index][1] = j;
-				index++;
-			}
-		
 		StringBuilder sb = new StringBuilder();
-		for(int game=0;game<4;game++) {
-			countries = new ArrayList<>();
+		for(int step=0;step<4;step++) {
 			st = new StringTokenizer(br.readLine());
 			
-			isAvailable = true;
-			
-			while(st.hasMoreTokens()) {
+			winArr = new int[6];
+			mmArr = new int[6];
+			loseArr = new int[6];
+			int totalWin = 0;
+			int totalMM = 0;
+			int totalLose = 0;
+			boolean check = true;
+			for(int i=0;i<6;i++) {
 				int win = Integer.parseInt(st.nextToken());
 				int mm = Integer.parseInt(st.nextToken());
 				int lose = Integer.parseInt(st.nextToken());
 				
-				if(win+mm+lose != 5) {
-					isAvailable = false;
+				if(win+mm+lose!=5) {
+					check = false;
 					break;
 				}
-				countries.add(new Country(win, mm, lose));
+				
+				totalWin += win;
+				totalMM += mm;
+				totalLose += lose;
+				
+				winArr[i] = win;
+				mmArr[i] = mm;
+				loseArr[i] = lose;
 			}
-			if(!isAvailable) {
-				sb.append("0 ");
+			if(totalWin != totalLose) check = false;
+			if(totalMM%2 != 0) check = false;
+			if(totalWin+totalLose+totalMM != 30) check = false;
+			
+			if(!check) {
+				sb.append(0+" ");
 				continue;
 			}
 			
-			isAvailable = false;
-			sol(0);
-			
-			sb.append((isAvailable?1:0)+" ");
+			if(backt(0))
+				sb.append(1+" ");
+			else
+				sb.append(0+" ");
 		}
 		System.out.println(sb);
 	}
-	static boolean isAvailable = false;
-	static void sol(int idx) {
-		if(idx==maxCnt) {
-			isAvailable = true;
-			return;
+	static boolean backt(int idx) {
+		if(idx==15) {
+			for(int i=0;i<6;i++) {
+				if(winArr[i]>0 || loseArr[i]>0 || mmArr[i]>0)
+					return false;
+			}
+			return true;
+		}
+
+		int teamA = pairs[idx][0];
+		int teamB = pairs[idx][1];
+		
+		if(winArr[teamA]>0 && loseArr[teamB]>0) {
+			winArr[teamA]--;
+			loseArr[teamB]--;
+			if(backt(idx+1))
+				return true;
+			winArr[teamA]++;
+			loseArr[teamB]++;
 		}
 		
-		Country a = countries.get(match[idx][0]);
-		Country b = countries.get(match[idx][1]);
-		
-		if(a.win>0 && b.lose>0) {
-			a.win--;
-			b.lose--;
-			sol(idx+1);
-			a.win++;
-			b.lose++;
+		if(loseArr[teamA]>0 && winArr[teamB]>0) {
+			loseArr[teamA]--;
+			winArr[teamB]--;
+			if(backt(idx+1))
+				return true;
+			loseArr[teamA]++;
+			winArr[teamB]++;
 		}
 		
-		if(a.mm>0 && b.mm>0) {
-			a.mm--;
-			b.mm--;
-			sol(idx+1);
-			a.mm++;
-			b.mm++;
+		if(mmArr[teamA]>0 && mmArr[teamB]>0) {
+			mmArr[teamA]--;
+			mmArr[teamB]--;
+			if(backt(idx+1))
+				return true;
+			mmArr[teamA]++;
+			mmArr[teamB]++;
 		}
 		
-		if(a.lose>0 && b.win>0) {
-			a.lose--;
-			b.win--;
-			sol(idx+1);
-			a.lose++;
-			b.win++;
-		}
+		return false;
 	}
 }
