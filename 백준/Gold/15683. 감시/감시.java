09230936b00cc70fbc4dd0f198169efc2d4427b5
karam Choi <86296569+karamChoi2523@users.noreply.github.com>
2025-09-05 16:24:13 +0900

    [Gold III] Title: 감시, Time: 68 ms, Memory: 11872 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.java
+++ 백준/Gold/15683. 감시/감시.java
@@ -5,21 +5,6 @@ public class Main {
 	//우 하 좌 상 우 하 좌 상 우 하
 	static int[] dx = {0,1,0,-1,0,1,0,-1,0,1};
 	static int[] dy = {1,0,-1,0,1,0,-1,0,1,0};
-	
-	static class Camera implements Comparable<Camera> {
-		int x, y, num;
-
-		public Camera(int x, int y, int num) {
-			this.x = x;
-			this.y = y;
-			this.num = num;
-		}
-		
-		@Override
-		public int compareTo(Camera o) {
-			return this.num - o.num;
-		}
-	}
 	static int N, M;
 	static int[][] board;	//0은 빈 칸, 6은 벽, 1~5는 CCTV의 번호
 	static ArrayList<Camera> cameras;
@@ -37,134 +22,132 @@ public class Main {
 		board = new int[N][M];
 		cameras = new ArrayList<>();
 		walls = new ArrayList<>();
-		visited = new boolean[N][M];
-		
 		for(int i=0;i<N;i++) {
 			st = new StringTokenizer(br.readLine());
 			for(int j=0;j<M;j++) {
 				board[i][j] = Integer.parseInt(st.nextToken());
-				if(board[i][j]!=0) {
-					if(board[i][j]==6)
-						walls.add(new int[] {i,j});
-					else
-						cameras.add(new Camera(i,j,board[i][j]));
-				}
+				if(board[i][j]>=1 && board[i][j]<=5)
+					cameras.add(new Camera(board[i][j],i,j));
+				else if(board[i][j]==6)
+					walls.add(new int[] {i,j});
 			}
 		}
 		min = Integer.MAX_VALUE;
-		sol();
+		sol(0);
 
 		System.out.println(min);
 	}
-	static void sol() {
+	static void sol(int idx) {
 		for(int d=0;d<4;d++) {
-			visited = new boolean[N][M];
-			int cnt = 0;
-			Collections.sort(cameras);
-			
-			for(int i=0;i<cameras.size();i++) {
-				Camera c = cameras.get(i);
-				rotate(c.x, c.y, c.num, d);
+			for(int step=0;step<2;step++) {
+				visited = new boolean[N][M];
+				if(step==0)	Collections.sort(cameras);
+				else Collections.reverse(cameras);
+				int cnt = 0;
+				
+				for(Camera c : cameras)
+					rotate(c.x, c.y, c.id, d);
+				
+				for(int[] wall : walls)
+					visited[wall[0]][wall[1]] = true;
+				
+				for(int i=0;i<N;i++)
+					for(int j=0;j<M;j++)
+						if(!visited[i][j])
+							cnt++;
+				min = Math.min(min, cnt);
 			}
-			for(int[] wall : walls)
-				visited[wall[0]][wall[1]] = true;
-			
-			for(int i=0;i<N;i++)
-				for(int j=0;j<M;j++)
-					if(!visited[i][j]) cnt++;
-			
-			min = Math.min(min, cnt);
-
-			visited = new boolean[N][M];
-			cnt = 0;
-			Collections.reverse(cameras);
-			for(int i=0;i<cameras.size();i++) {
-				Camera c = cameras.get(i);
-				rotate(c.x, c.y, c.num, d);
-			}
-			for(int[] wall : walls)
-				visited[wall[0]][wall[1]] = true;
-			
-			for(int i=0;i<N;i++)
-				for(int j=0;j<M;j++)
-					if(!visited[i][j]) cnt++;
-			
-			min = Math.min(min, cnt);
 		}
 	}
-	static void rotate(int x, int y, int num, int dir) {
+	private static void rotate(int x, int y, int id, int dir) {
 		int max = -1;
 		int mIdx = -1;
-		if(num==1) {
-			for(int i=dir;i<dir+4;i++) {
-				int curr = count(x,y,i);
-				if(max<curr) {
+		
+		if(id==1) {
+			for(int d=dir;d<dir+4;d++) {
+				int curr = count(x,y,d);
+				if(curr>max) {
 					max = curr;
-					mIdx = i;
+					mIdx = d;
 				}
 			}
-			flag(x, y, mIdx);
-		}else if(num==2) {
-			for(int i=dir;i<dir+2;i++) {
-				int curr = count(x,y,i) + count(x,y,i+2);
-				if(max<curr) {
+			checkVisit(x, y, mIdx);
+		}else if(id==2) {
+			for(int d=dir;d<dir+2;d++) {
+				int curr = count(x,y,d)+count(x,y,d+2);
+				if(curr>max) {
 					max = curr;
-					mIdx = i;
+					mIdx = d;
 				}
 			}
-			flag(x, y, mIdx);
-			flag(x, y, mIdx+2);
-		}else if(num==3) {
-			for(int i=dir;i<dir+4;i++) {
-				int curr = count(x,y,i) + count(x,y,i+1) ;
-				if(max<curr) {
+			checkVisit(x,y,mIdx);
+			checkVisit(x,y,mIdx+2);
+		}else if(id==3) {
+			for(int d=dir;d<dir+4;d++) {
+				int curr = count(x,y,d)+count(x,y,d+1);
+				if(curr>max) {
 					max = curr;
-					mIdx = i;
+					mIdx = d;
 				}
 			}
-			flag(x, y, mIdx);
-			flag(x, y, mIdx+1);
-		}else if(num==4) {
-			for(int i=dir;i<dir+4;i++) {
-				int curr = count(x,y,i) + count(x,y,i+1) + count(x,y,i+2);
-				if(max<curr) {
+			checkVisit(x,y,mIdx);
+			checkVisit(x,y,mIdx+1);
+		}else if(id==4) {
+			for(int d=dir;d<dir+4;d++) {
+				int curr = count(x,y,d)+count(x,y,d+1)+count(x,y,d+2);
+				if(curr>max) {
 					max = curr;
-					mIdx = i;
+					mIdx = d;
 				}
 			}
-			flag(x, y, mIdx);
-			flag(x, y, mIdx+1);
-			flag(x, y, mIdx+2);
-		}else {
-			flag(x, y, 0);
-			flag(x, y, 1);
-			flag(x, y, 2);
-			flag(x, y, 3);
+			checkVisit(x,y,mIdx);
+			checkVisit(x,y,mIdx+1);
+			checkVisit(x,y,mIdx+2);
+		}else if(id==5) {
+			checkVisit(x,y,0);
+			checkVisit(x,y,1);
+			checkVisit(x,y,2);
+			checkVisit(x,y,3);
 		}
 	}
-	static int count(int x, int y, int d) {
+	private static void checkVisit(int x, int y, int d) {
+		visited[x][y] = true;
+		while(true) {
+			x += dx[d];
+			y += dy[d];
+			if(!checkNext(x,y)||board[x][y]==6) break;
+			if(board[x][y]==0 && !visited[x][y])
+				visited[x][y] = true;
+		}
+	}
+	private static int count(int x, int y, int d) {
 		int cnt = 0;
 		while(true) {
 			x += dx[d];
 			y += dy[d];
 			
-			if(x<0 || x>=N || y<0 || y>=M) break;
-			if(board[x][y]==6) break;
-			
-			if(board[x][y]==0 && !visited[x][y]) cnt++;
+			if(!checkNext(x,y)||board[x][y]==6) break;
+			if(board[x][y]==0 && !visited[x][y])
+				cnt++;
 		}
 		return cnt;
 	}
-	static void flag(int x, int y, int d) {
-		visited[x][y] = true;
-		while(true) {
-			x += dx[d];
-			y += dy[d];
-			
-			if(x<0 || x>=N || y<0 || y>=M) break;
-			if(board[x][y]==6) break;
-			
-			if(board[x][y]==0 && !visited[x][y]) visited[x][y] = true;
+	
+	static boolean checkNext(int x, int y) {
+		if(x<0 || x>=N || y<0 || y>=M) return false;
+		return true;
+	}
+	static class Camera implements Comparable<Camera>{
+		int id, x, y;
+
+		public Camera(int id, int x, int y) {
+			this.id = id;
+			this.x = x;
+			this.y = y;
+		}
+		@Override
+		public int compareTo(Camera o) {
+			return this.id-o.id;
 		}
 	}
 }
