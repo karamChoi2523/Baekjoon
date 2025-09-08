e31489ee23f31bfe026673a9c35165a27c5f6e80
karam Choi <86296569+karamChoi2523@users.noreply.github.com>
2025-09-08 17:37:02 +0900

    [level 3] Title: 양과 늑대, Time: 3.69 ms, Memory: 91.7 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -1,58 +1,28 @@
-import java.util.*;
-
 class Solution {
-    static int N, answer;
-    static int[] infoArr;
-    static List<Integer>[] graph;
-    static boolean[] visited;
-
-    public int solution(int[] info, int[][] edges) {
-    	N = info.length;
-    	infoArr = info.clone();
-        graph = new ArrayList[N];
+    static int answer = 0;
+    
+	public int solution(int[] info, int[][] edges) {
+        dfs(0, new boolean[info.length], 0,0, info, edges);
         
-        for (int i = 0; i < N; i++)
-        	graph[i] = new ArrayList<>();
-        for (int[] edge : edges) {
-            graph[edge[0]].add(edge[1]);
-            graph[edge[1]].add(edge[0]);
-        }
-
-        answer = 0;
-        visited = new boolean[N];
-
-        visited[0] = true;
-        ArrayList<Integer> cand = new ArrayList<>();
-        for (int v : graph[0])
-        	cand.add(v);
-
-        dfs(info[0]==0?1:0, info[0]==1?1:0, cand);
         return answer;
     }
-    static void dfs(int sheep, int wolf, ArrayList<Integer> cand) {
-        answer = Math.max(answer, sheep);
 
-        for (int i = 0; i < cand.size(); i++) {
-            int v = cand.get(i);
-            if (visited[v]) continue;
+    static void dfs(int idx, boolean[] visited, int sheep, int wolf, int[] info, int[][] edges) {
+    	visited[idx] = true;
 
-            int ns = sheep + (infoArr[v] == 0 ? 1 : 0);
-            int nw = wolf  + (infoArr[v] == 1 ? 1 : 0);
-            if (ns <= nw) continue;
-
-            visited[v] = true;
-
-            ArrayList<Integer> nextCand = new ArrayList<>(cand);
-            nextCand.remove(i);
-            for (int next : graph[v]) {
-                if (!visited[next])
-                	nextCand.add(next);
-            }
-
-            dfs(ns, nw, nextCand);
-
-            visited[v] = false;
+        if(info[idx]==0) {
+        	sheep += 1;
+        	answer = Math.max(sheep, answer);
+        }else
+        	wolf+=1;
+        
+        if(sheep<=wolf) return;
+        
+        for(int[] edge : edges) {
+        	if(visited[edge[0]] && !visited[edge[1]]) {
+        		boolean[] visit = visited.clone();
+        		dfs(edge[1], visit, sheep, wolf, info, edges);
+        	}
         }
     }
-
 }
