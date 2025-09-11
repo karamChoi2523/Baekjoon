49678e2aa48ec6f3e4133b1ac5b740aeb4d55bd7
karam Choi <86296569+karamChoi2523@users.noreply.github.com>
2025-09-11 09:32:47 +0900

    [D4] Title: [Professional] 키 순서, Time: 774 ms, Memory: 100,460 KB -BaekjoonHub

--- SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
+++ SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
@@ -15,41 +15,30 @@ public class Solution {
 			
 			int[][] graph = new int[N+1][N+1];
 			for(int i=1;i<N+1;i++)
-				Arrays.fill(graph[i],(int)1e9);
+				Arrays.fill(graph[i], (int)1e9);
 			
 			for(int i=0;i<M;i++) {
 				st = new StringTokenizer(br.readLine());
 
 				int a = Integer.parseInt(st.nextToken());
 				int b = Integer.parseInt(st.nextToken());
-				
+			
 				graph[a][b] = 1;
 			}
 			
 			for(int k=1;k<N+1;k++)
-				for(int i=1;i<N+1;i++) {
-					if(k==i) continue;
-					for(int j=1;j<N+1;j++) {
-						if(i==j || j==k) continue;
-						if(graph[i][j] > graph[i][k] + graph[k][j])
-							graph[i][j] = graph[i][k] + graph[k][j];
-					}
-				}
-			
+				for(int i=1;i<N+1;i++)
+					for(int j=1;j<N+1;j++)
+						graph[i][j] = Math.min(graph[i][j], graph[i][k]+graph[k][j]);
 			int answer = 0;
-			int[] count = new int[N+1];
-			for(int i=1;i<N+1;i++)
-				for(int j=1;j<N+1;j++) {
-					if(graph[i][j]!=(int)1e9)
-						count[i]++;
-					
-					if(graph[j][i]!=(int)1e9)
-						count[i]++;
-				}
-			
-			for(int i=1;i<N+1;i++)
-				if(count[i]==N-1)
+			for(int i=1;i<N+1;i++) {
+				int cnt = 0;
+				for(int j=1;j<N+1;j++)
+					if(graph[i][j]!=(int)1e9 || graph[j][i]!=(int)1e9)
+						cnt++;
+				if(cnt==N-1)
 					answer++;
+			}
 			
 			System.out.printf("#%d %d\n", tc, answer);
 		}
