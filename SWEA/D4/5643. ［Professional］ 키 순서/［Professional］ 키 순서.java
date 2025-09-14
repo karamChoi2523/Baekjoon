43c85f8715276de9bb65db9fb9f00447fb3418f8
karam Choi <86296569+karamChoi2523@users.noreply.github.com>
2025-09-14 15:30:24 +0900

    [D4] Title: [Professional] 키 순서, Time: 760 ms, Memory: 102,164 KB -BaekjoonHub

--- SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
+++ SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
@@ -1,84 +1,44 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Solution {
-	static int N;
-	static int[][] graph;
-	static int count;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
 		StringTokenizer st;
-		
+
 		int T = Integer.parseInt(br.readLine());
 		for(int tc=1;tc<=T;tc++) {
-			
-			N = Integer.parseInt(br.readLine());
+			int N = Integer.parseInt(br.readLine());
 			int M = Integer.parseInt(br.readLine());
-			
-			graph = new int[N+1][N+1];
-			
-			//1. 메모 안 된 상태로 초기화
+
+			int[][] adj = new int[N+1][N+1];
 			for(int i=1;i<N+1;i++)
-				graph[i][0] = -1;
-			
+				Arrays.fill(adj[i], (int)1e9);
+
 			for(int i=0;i<M;i++) {
 				st = new StringTokenizer(br.readLine());
-
 				int a = Integer.parseInt(st.nextToken());
 				int b = Integer.parseInt(st.nextToken());
-			
-				graph[a][b] = 1;	//a보다 b가 크다
+				adj[a][b] = 1;
 			}
-			
-			//탐색 여부 memo
-			// counting만 하는 게 아니라 graph에 저장
-			//i의 0열 체크 - 나보다 큰 학생 저장
-			//i의 행 체크 - 나보다 작은 학생 저장
-			
-			//모든 학생에 대해 자신보다 키가 큰 학생 탐색
-			//이 과정에서 간접관계를 직접 관계로 경로 압축
-			for(int i=1;i<N+1;i++) {
-				if(graph[i][0] == -1)	//-1이면(memo가 안 되어있으면) 탐색
-					gtDFS(i);
-			}
-			
-			//자신보다 작은 학생 탐색
-			for(int i=1;i<N+1;i++) {
-				for(int j=1;j<N+1;j++)
-					graph[0][i] += graph[j][i];
-			}
-			
-			int answer = 0;
-			for(int i=1;i<N+1;i++)
-				if(graph[i][0]+graph[0][i]==N-1)
-					answer++;
-			
-			
-			System.out.printf("#%d %d\n", tc, answer);
-		}
-	}
-	static void gtDFS(int curr) {
-		for(int i=1;i<N+1;i++)
-			if(graph[curr][i]==1) {	//curr<i라면
-				if(graph[i][0]==-1)	//i가 탐색 전이면 탐색
-					gtDFS(i);
-				//i가 탐색을 이미 했거나 아님 탐색을 마쳐서 이 위치로 옴
-				//관계를 curr에 반영
-				if(graph[i][0]>0){ //i보다 큰 학생이 있다면 => curr<i<j => curr<j로 표현
+
+			for(int k=1;k<N+1;k++)
+				for(int i=1;i<N+1;i++)
 					for(int j=1;j<N+1;j++)
-						if(graph[i][j]==1)	//i<j인 학생j를 찾아 curr<j로 표현
-							graph[curr][j] = 1;
+						adj[i][j] = Math.min(adj[i][j], adj[i][k]+adj[k][j]);
+
+			int answer = 0;
+			for(int i=1;i<N+1;i++) {
+				int cnt = 0;
+				for(int j=1;j<N+1;j++) {
+					if(i==j) continue;
+					if(adj[i][j]!=(int)1e9||adj[j][i]!=(int)1e9)
+						cnt++;
 				}
+				if(cnt==N-1)
+					answer++;
 			}
-		//자신보다 큰 학생들 후 카운팅 후 memo
-		//처음에 graph[i][0]만 -1로 초기화했으므로
-		//graph[0][i]는 0
-		int cnt = 0;
-		for(int i=1;i<N+1;i++) {
-			if(graph[curr][i]>0)
-				++cnt;
+			System.out.printf("#%d %d\n",tc, answer);
 		}
-		graph[curr][0] = cnt;
 	}
 }
