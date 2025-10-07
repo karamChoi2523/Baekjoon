a75bdb3da93fbb507410a7ef05512a29b9637432
karam Choi <86296569+karamChoi2523@users.noreply.github.com>
2025-10-08 01:05:36 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 9.30 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
@@ -2,41 +2,60 @@ import java.util.*;
 
 class Solution {
     public int solution(String[] friends, String[] gifts) {
-        int answer = 0;
+        	        int answer = 0;
+	        
+	        int size = friends.length;
+	        int[][] graph = new int[size][size];
+	        Map<String, Integer> map = new HashMap<>();
+	        
+	        for(int i=0;i<friends.length;i++)
+	        	map.put(friends[i], i);
+	        
+	        for(String temp : gifts) {
+	        	String[] t = temp.split(" ");
+	        	
+	        	int a = map.get(t[0]);
+	        	int b = map.get(t[1]);
+	        	
+	        	graph[a][b]++;	//a가 b에게
+	        }
+	        
+	        int[] nextMonth = new int[size];
+        int[] score = new int[size];
         
-        Map<String,Integer> map = new HashMap<>();
-        for(int i=0;i<friends.length;i++)
-        	map.put(friends[i], i);
-     
-        int[][] board = new int[friends.length][friends.length];
-        int[] index = new int[friends.length];	//선물지수
-        
-        for(String record : gifts) {
-        	String[] rr = record.split(" ");
-        	
-        	index[map.get(rr[0])]++;
-        	index[map.get(rr[1])]--;
-        	board[map.get(rr[0])][map.get(rr[1])]++;
-        }
-        
-        for(int i=0;i<friends.length;i++) {
-        	int cnt = 0;
-        	for(int j=0;j<friends.length;j++) {
-        		if(i!=j) {
-        			int a = board[i][j];
-        			int b = board[j][i];
-        			
-        			
-        			if(a>b)
-        				cnt++;
-        			else if(board[i][j]==board[j][i] && index[i]>index[j])
-        				cnt++;
-        		}
-        		answer = Math.max(answer, cnt);
-        	}
-        }
-        
-        
-        return answer;
+        for(int i=0;i<size;i++)
+        	score[i] = calScore(graph, i, size);
+	        
+	        for(int i=0;i<size;i++) {
+	        	for(int j=i+1;j<size;j++) {
+	        		if(graph[i][j]!=graph[j][i]) {
+	        			if(graph[i][j]>graph[j][i]) nextMonth[i]++;
+	        			else nextMonth[j]++;
+	        		}else {
+                        int a = score[i];
+                        int b = score[j];
+	        			
+	        			if(a>b) nextMonth[i]++;
+	        			else if(a<b) nextMonth[j]++;
+	        		}
+	        	}
+	        }
+	        
+	        for(int i=0;i<size;i++)
+	        	answer = Math.max(answer, nextMonth[i]);
+	        
+	        return answer;
     }
+
+		static private int calScore(int[][] graph, int target, int size) {
+			int give = 0, get = 0;
+			
+			for(int i=0;i<size;i++) {
+				if(i==target) continue;
+				
+				give += graph[target][i];
+				get += graph[i][target];
+			}
+			return give-get;
+		}
 }
