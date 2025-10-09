f1d47c3245bbb9f7c973467854c380be26c14976
karam Choi <86296569+karamChoi2523@users.noreply.github.com>
2025-10-09 21:58:49 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 31.45 ms, Memory: 175 MB -BaekjoonHub

--- 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.java
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.java
@@ -1,32 +1,31 @@
-import java.util.*;
-
 class Solution {
+	static final int max = (int)1e6+1;
     public int[] solution(int[][] edges) {
-        int[] answer = {};
-		
-		int[] ingoing = new int[(int)1e6+1];
-        int[] outgoing = new int[(int)1e6+1];
-        
-        for(int[] edge : edges) {
-            outgoing[edge[0]]++;
-            ingoing[edge[1]]++;
-        }
-        
+        int[] answer = {};	//정점 번호, 도넛, 막대, 8자
         answer = new int[4];
         
-        for(int i=1; i<(int)1e6; i++) {
-            if(outgoing[i] >= 2) {
-                if(ingoing[i] == 0)
-                    answer[0] = i;
-                else
-                    answer[3]++;
-                
-            }else if(outgoing[i]==0 && ingoing[i]>=1){
-                answer[2]++;
-            }
+        int size = edges.length;
+        int[] ingoing = new int[max];
+        int[] outgoing = new int[max];
+        
+        for(int[] edge : edges) {
+        	outgoing[edge[0]]++;
+        	ingoing[edge[1]]++;
+        }
+        
+        for(int i=1;i<max;i++) {
+        	if(outgoing[i]>=2) {
+        		if(ingoing[i]==0) answer[0] = i;
+        		else answer[3]++;
+        	}else {
+        		if(outgoing[i]==0 && ingoing[i]>=1)
+        			answer[2]++;
+        	}
         }
         
         answer[1] = outgoing[answer[0]]-(answer[2]+answer[3]);
+        
+        
         return answer;
     }
 }
