459065c3d862c0f49c0e4f893708d96feec1b84f
karam Choi <86296569+karamChoi2523@users.noreply.github.com>
2026-04-13 22:42:20 +0900

    [Gold V] Title: 누가 이길까, Time: 300 ms, Memory: 39676 KB -BaekjoonHub

--- 백준/Gold/28449. 누가 이길까/누가 이길까.java
+++ 백준/Gold/28449. 누가 이길까/누가 이길까.java
@@ -2,14 +2,14 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	static long N,M;
+	static int N,M;
 	static int[] t1;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Long.parseLong(st.nextToken());
-		M = Long.parseLong(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
 		
 		t1 = new int[100001];
 		
@@ -21,16 +21,17 @@ public class Main {
 		for(int i=1;i<100001;i++)
 			t1[i] += t1[i-1];
 		
-		long win = 0, lose=0;
+		long win = 0, draw=0, lose=0;
 		
 		st = new StringTokenizer(br.readLine());
 		for(int i=0;i<M;i++) {
 			int index = Integer.parseInt(st.nextToken());
 			win += N-t1[index];
 			lose += t1[index-1];
+			draw += t1[index]-t1[index-1];
 		}
 		
 		//t1 승리 횟수, t2 승리 횟수, 무승부 횟수
-		System.out.println(win+" "+lose+" "+(N*M-win-lose));
+		System.out.println(win+" "+lose+" "+draw);
 	}
 }
