d0a46866af6973f08f8a0830ec780b22a83720e5
karam Choi <86296569+karamChoi2523@users.noreply.github.com>
2026-04-13 23:20:52 +0900

    [Gold V] Title: 누가 이길까, Time: 288 ms, Memory: 38324 KB -BaekjoonHub

--- 백준/Gold/28449. 누가 이길까/누가 이길까.java
+++ 백준/Gold/28449. 누가 이길까/누가 이길까.java
@@ -2,36 +2,35 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	static int N,M;
-	static int[] t1;
+	static int N, M;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		
-		t1 = new int[100001];
+		int[] dp = new int[100001];
 		
 		st = new StringTokenizer(br.readLine());
 		for(int i=0;i<N;i++) {
-			int index = Integer.parseInt(st.nextToken());
-			t1[index]++;
+			int n = Integer.parseInt(st.nextToken());
+			dp[n]++;
 		}
+		
 		for(int i=1;i<100001;i++)
-			t1[i] += t1[i-1];
+			dp[i] += dp[i-1];
 		
-		long win = 0, draw=0, lose=0;
-		
+		long win=0, lose=0, draw=0;
 		st = new StringTokenizer(br.readLine());
 		for(int i=0;i<M;i++) {
-			int index = Integer.parseInt(st.nextToken());
-			win += N-t1[index];
-			lose += t1[index-1];
-			draw += t1[index]-t1[index-1];
+			int n = Integer.parseInt(st.nextToken());
+			win += N-dp[n];
+			lose += dp[n-1];
+			draw += dp[n]-dp[n-1];
 		}
 		
-		//t1 승리 횟수, t2 승리 횟수, 무승부 횟수
 		System.out.println(win+" "+lose+" "+draw);
 	}
 }
