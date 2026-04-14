8d475d8b47238b29f68d5fa4c8e5ab8ace7c1143
karam Choi <86296569+karamChoi2523@users.noreply.github.com>
2026-04-14 21:17:50 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 472 ms, Memory: 51236 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -2,9 +2,12 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+	//크루스칼
+	static int V, E;
+	static int[] parent;
+	
 	static class Edge implements Comparable<Edge>{
-		int a;
-		int b;
+		int a, b;
 		long cost;
 		
 		public Edge(int a, int b, long c) {
@@ -12,64 +15,69 @@ public class Main {
 			this.b = b;
 			cost = c;
 		}
-		
+
 		@Override
 		public int compareTo(Edge o) {
-			return (int)(this.cost-o.cost);
+			return (int)(this.cost - o.cost);
 		}
+		
+		
 	}
-
-	static int[] parent;
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int V = Integer.parseInt(st.nextToken());
-		int E = Integer.parseInt(st.nextToken());
+		V = Integer.parseInt(st.nextToken());	//정점 개수
+		E = Integer.parseInt(st.nextToken());	//간선의 수
 		
+		//초기에 부모 = 나 자신
 		parent = new int[V+1];
-		
-		for(int i=1;i<V+1;i++) {
+		for(int i=1;i<V+1;i++)
 			parent[i] = i;
-		}
 		
+		// 가중치에 따라 오름차순 정렬
 		PriorityQueue<Edge> pq = new PriorityQueue<>();
+		
 		for(int i=0;i<E;i++) {
 			st = new StringTokenizer(br.readLine());
-
-			int A = Integer.parseInt(st.nextToken());
-			int B = Integer.parseInt(st.nextToken());
-			long C = Long.parseLong(st.nextToken());
 			
-			if(A<=B)
-				pq.add(new Edge(A,B,C));
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			long c = Long.parseLong(st.nextToken());
+			
+			if(a<=b)
+				pq.add(new Edge(a, b, c));
 			else
-				pq.add(new Edge(B,A,C));
+				pq.add(new Edge(b, a, c));
 		}
 		
 		long answer = 0;
+		
 		while(!pq.isEmpty()) {
 			Edge curr = pq.poll();
 			
 			if(findParent(curr.a)!=findParent(curr.b)) {
-				union(curr.a,curr.b);
+				union(curr.a, curr.b);
 				answer += curr.cost;
 			}
 		}
+		
 		System.out.println(answer);
 	}
-	static void union(int x, int y) {
-		int a = findParent(x);
-		int b = findParent(y);
-		
-		if(a!=b)
-			parent[b] = a;
-	}
-	
 	static int findParent(int x) {
-		if(parent[x]==x) return x;
+		while(x!=parent[x]) {
+			parent[x] = parent[parent[x]];
+			x = parent[x];
+		}
 		
-		return parent[x] = findParent(parent[x]);
+		return x;
+	}
+	static void union(int x, int y) {
+		int px = findParent(x);
+		int py = findParent(y);
+		
+		if(px<=py) parent[py] = px;
+		else parent[px] = py;
 	}
 }
